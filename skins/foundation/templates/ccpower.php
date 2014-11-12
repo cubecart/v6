127fd387cc9415bd5bb3bd2dd39fe0925d052b89
Al Brookbanks <al@fathem.com>
2014-11-12 17:30:43 +0000

    guilt trip

--- skins/foundation/templates/ccpower.php
+++ skins/foundation/templates/ccpower.php
@@ -1,7 +1,8 @@
 {*
-	<guilt_trip>
-	A HUGE amount of work has gone into the development of CubeCart.
-	Be nice and consider leaving a link back to us. ♥ 
-	</guilt_trip> 
+	<note description="shameless guilt trip">
+	A HUGE amount of work has gone into the development of CubeCart ..
+	.. our success = your success!! 
+	You could maybe, just possibly leave this link just as it is!? ♥ 
+	</note>
 *}
 <p>eCommerce by <a href="http://www.cubecart.com">CubeCart</a></p>
