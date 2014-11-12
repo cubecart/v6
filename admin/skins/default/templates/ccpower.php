36e985da017e13c9bef7477033ad203e1e261236
Al Brookbanks <al@fathem.com>
2014-11-12 17:37:01 +0000

    link text

--- admin/skins/default/templates/ccpower.php
+++ admin/skins/default/templates/ccpower.php
@@ -1,8 +1,7 @@
 {*
 	<note description="shameless guilt trip">
-	A HUGE amount of work has gone into the development of CubeCart ..
-	.. our success = your success!! 
-	You could maybe, just possibly leave this link just as it is!? ♥ 
+	Blood, sweat, tears and not to mention time (buckets of it) has gone into the development of CubeCart.
+	You could maybe.. just possibly.. leave a little linky to us!? ♥ x 
 	</note>
 *}
 <div class="powered">eCommerce by <a href="http://www.cubecart.com">CubeCart</a></div>
