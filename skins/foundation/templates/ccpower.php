7b1fce692e61130258e6b94806d6a5e1fe3570d2
Al Brookbanks <al@fathem.com>
2014-11-13 08:20:06 +0000

    Note updated

--- skins/foundation/templates/ccpower.php
+++ skins/foundation/templates/ccpower.php
@@ -1,7 +1,7 @@
 {*
 	<note description="shameless guilt trip">
-	Blood, sweat, tears and not to mention time (buckets of it) has gone into the development of CubeCart.
-	You could maybe.. just possibly.. leave a little linky to us!? ♥ x 
+	A vast amount of time has gone into the development of CubeCart.
+	You could maybe.. just possibly.. leave a little link back to us!? ♥ x 
 	</note>
 *}
 <p>eCommerce by <a href="http://www.cubecart.com">CubeCart</a></p>
