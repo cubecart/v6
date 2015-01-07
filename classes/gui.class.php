e96ee1fd33c8dcf513615e0298bde28d7e9c8373
Al Brookbanks <al@fathem.com>
2015-01-07 13:00:27 +0000

    Third fix for issue #20

--- classes/gui.class.php
+++ classes/gui.class.php
@@ -1343,7 +1343,8 @@ class GUI {
 		}
 
 		if (empty($this->_skin)) {
-			$GLOBALS['config']->set('config', 'skin_folder'.$this->_skin_config_postfix, 'foundation');
+			$this->_skin = 'foundation';
+			$GLOBALS['config']->set('config', 'skin_folder'.$this->_skin_config_postfix, $this->_skin);
 		}
 
 		if (($custom = $GLOBALS['cache']->read('skin.custom')) === false && file_exists(CC_ROOT_DIR.'/skins/'.$this->_skin.'/'.'config.xml')) {
