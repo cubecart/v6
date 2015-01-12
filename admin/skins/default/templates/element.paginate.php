4acce0a2b28b2752020d51f8cf0d2e2f7305e165
Al Brookbanks <al@fathem.com>
2015-01-12 17:25:03 +0000

    Revert b044d16..d37608f
    This rolls back to commit b044d169ccf40e4412eb1c95dd02f7d8bb5270c6.

--- admin/skins/default/templates/element.paginate.php
+++ admin/skins/default/templates/element.paginate.php
@@ -9,23 +9,23 @@
  * Email:  sales@cubecart.com
  * License:  GPL-3.0 https://www.gnu.org/licenses/quick-guide-gplv3.html
  *}
-{if ($page gte $show-1)}
+{if ($page >= $show-1)}
   {$params[$var_name] = 1}
   <a href="{$current}{
   ($params)}{$anchor}">1</a> &hellip;
 {/if}
 
-{if ($page gt 1)}
+{if ($page > 1)}
   {$params[$var_name] = $page-1}
   <a href="{$current}{http_build_query($params)}{$anchor}">&lt;</a>
 {/if}
 
-{for $i = 1; $i lte $total; $i++}
-	{if ($i lt $page - floor($show / 2))}
+{for $i = 1; $i <= $total; $i++}
+	{if ($i < $page - floor($show / 2))}
 	{continue}
 	{/if}
 
-	{if ($i gt $page + floor($show / 2))}
+	{if ($i > $page + floor($show / 2))}
 	{break}
 	{/if}
 
@@ -37,12 +37,12 @@
   	{/if}
 {/for}
 
-{if ($page lt $total)}
+{if ($page < $total)}
   {$params[$var_name] = $page + 1}
   <a href="{$current}{http_build_query($params)}{$anchor}">&gt;</a>
 {/if}
 
-{if ($i lte $total)}
+{if ($i <= $total)}
   {$params[$var_name] = $total}
   &hellip; <a href="{$current}{http_build_query($params)}{$anchor}">{$total}</a>
 {/if}
