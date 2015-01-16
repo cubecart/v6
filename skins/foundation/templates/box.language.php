d6a63a93901ea1ce94ba9348f2b8716acf3775de
Al Brookbanks <al@fathem.com>
2015-01-16 11:02:57 +0000

    Fix for issue #45

--- skins/foundation/templates/box.language.php
+++ skins/foundation/templates/box.language.php
@@ -11,11 +11,11 @@
  *}
 {if $LANGUAGES}
 <div class="right text-center show-for-medium-up" id="box-language">
-   <a href="#" data-dropdown="language-switch" class="button white small"><img class="flag flag-{$current_language.code|substr:3:2}" alt="{$current_language.title}" /></a><br>
+   <a href="#" data-dropdown="language-switch" class="button white small"><img class="flag flag-{$current_language.code|substr:3:2}" alt="{$current_language.title}" src="#"></a><br>
    <ul id="language-switch" data-dropdown-content class="f-dropdown">
       {foreach from=$LANGUAGES item=language}
       {if $current_language.code!==$language.code}
-      <li class="text-left"><a href="{$language.url}"><img src="#" class="flag flag-{$language.code|substr:3:2}" alt="{$language.title}" /> {$language.title}</a></li>
+      <li class="text-left"><a href="{$language.url}"><img src="#" class="flag flag-{$language.code|substr:3:2}" alt="{$language.title}"> {$language.title}</a></li>
       {/if}
       {/foreach}
    </ul>
