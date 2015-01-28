91346225f85b3e94ba7293624dcae889e9c46d7e
Al Brookbanks <al@fathem.com>
2015-01-28 11:00:57 +0000

    Improvements to #118

--- admin/skins/default/templates/maintenance.index.php
+++ admin/skins/default/templates/maintenance.index.php
@@ -17,9 +17,10 @@
   </fieldset>
   <fieldset><legend>{$LANG.settings.title_seo_urls}</legend>
 	<div><label for="truncate_seo">{$LANG.maintain.truncate_seo}</label><span><input type="checkbox" id="truncate_seo" name="truncate_seo" value="1"></span></div>
+	<div class="clear important"><strong>{$LANG.maintain.truncate_seo_warning}</strong></div>
   </fieldset>
   <fieldset><legend>{$LANG.maintain.title_rebuild_cache}</legend>
-	<div><label for="clearCache">{$LANG.maintain.cache_clear}</label><span><input type="checkbox" id="clearCache" name="clearCache" value="1"></span><!--{$LANG.maintain.cache_warning}--></div>
+	<div><label for="clearCache">{$LANG.maintain.cache_clear}</label><span><input type="checkbox" id="clearCache" name="clearCache" value="1"></span></div>
 	<div><label for="clearSQLCache">{$LANG.maintain.cache_sql}</label><span><input type="checkbox" id="clearSQLCache" name="clearSQLCache" value="1"></span></div>
 	<div><label for="clearLangCache">{$LANG.maintain.cache_language}</label><span><input type="checkbox" id="clearLangCache" name="clearLangCache" value="1"></span></div>
 	<div><label for="clearImageCache">{$LANG.maintain.cache_image}</label><span><input type="checkbox" id="clearImageCache" name="clearImageCache" value="1"></span></div>
