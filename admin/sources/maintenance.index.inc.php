86e12240d23c71c40ce7bb4e491baacf7794d94e
Al Brookbanks <al@fathem.com>
2015-02-02 10:43:24 +0000

    Fixed issue #157

--- admin/sources/maintenance.index.inc.php
+++ admin/sources/maintenance.index.inc.php
@@ -287,7 +287,7 @@ if (isset($_GET['upgrade']) && !empty($_GET['upgrade'])) {
 if (isset($_GET['delete']) && file_exists('backup/'.$_GET['delete'])) {
 	## Generic error message for logs delete specific for backup
 	$message = preg_match('/\_error_log$/', $_GET['delete']) ? $lang['filemanager']['notify_file_delete'] : $lang['maintain']['backup_deleted'];
-	$GLOBALS['main']->setACPWarning();
+	$GLOBALS['main']->setACPWarning($message);
 	unlink('backup/'.$_GET['delete']);
 	httpredir('?_g=maintenance&node=index#backup');
 }
