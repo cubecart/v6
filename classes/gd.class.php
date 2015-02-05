0ebbdbd031e946ff6541e8fc4d74320b871580fe
Al Brookbanks <al@fathem.com>
2015-02-05 16:59:54 +0000

    Stripped GD class right back

--- classes/gd.class.php
+++ classes/gd.class.php
@@ -25,10 +25,8 @@ class GD {
 	private $_gdImageSource;
 	private $_gdImageOutput;
 
-	private $_gdWatermark;
 	private $_gdUploaded = false;
 
-	##############################################
 
 	public function __construct($targetDir, $maxImage = false, $jpegQuality = 100) {
 		if (substr($targetDir, -1) != '/') {
@@ -39,16 +37,55 @@ class GD {
 		$this->_gdJpegQuality = $jpegQuality;
 	}
 
+	/**
+	 * Set GD params false
+	 */
 	public function gdClear() {
 		$this->_gdImageOutput = false;
 		$this->_gdImageSource = false;
 		$this->_gdImageData  = false;
 		$this->_gdImageExif  = false;
-		$this->_gdWatermark  = false;
 	}
 
-	##############################################
+	/**
+	 * Crop image
+	 *
+	 * @param int $x
+	 * @param int $y
+	 * @param int $w
+	 * @param int $h
+	 */
+	public function gdCrop($x, $y, $w, $h) {
+		if ($im = $this->gdGetCurrentData()) {
+			$oh = imagesy($im);
+			$ow = imagesx($im);
+			$h = ($oh < $h) ? $oh : $h;
+			$w = ($ow < $w) ? $ow : $w;
+			$this->_gdImageOutput = imagecreatetruecolor($w, $h);
+			imagecopyresampled($this->_gdImageOutput, $im, 0, 0, $x, $y, $w, $h, $w, $h);
+			imagesavealpha($this->_gdImageOutput, true);
+			$this->_gdImageArray[0] = $w;
+			$this->_gdImageArray[1] = $h;
+		}
+	}
 
+	/**
+	 * Return image output
+	 *
+	 * @return data
+	 */
+	private function gdGetCurrentData() {
+		// Detect what data source we should be using
+		// If output is empty, use the source
+		return (!empty($this->_gdImageOutput)) ? $this->_gdImageOutput : $this->_gdImageSource;
+	}
+
+	/**
+	 * Load file
+	 *
+	 * @param string $file
+	 * @return bool
+	 */
 	public function gdLoadFile($file) {
 		if (file_exists($file)) {
 			$this->_gdImageData = getimagesize($file);
@@ -76,105 +113,11 @@ class GD {
 		return false;
 	}
 
-	public function gdSave($filename, $resize = false, $thumbnail = false) {
-		// Do we need to resize the file before saving?
-		if ($resize || $this->_gdImageMax) {
-			$this->gdResize(($resize) ? $resize : $this->_gdImageMax);
-		}
-		$im = $this->gdGetCurrentData();
-		if ($im) {
-			$file = $this->_gdTargetDir.$filename;
-			imageinterlace($im, true);
-			switch ($this->_gdImageType) {
-			case IMAGETYPE_GIF:
-				$this->_gdImageSource = imagegif($im, $file);
-				break;
-			case IMAGETYPE_JPEG:
-				$this->_gdImageSource = imagejpeg($im, $file, $this->_gdJpegQuality);
-				break;
-			case IMAGETYPE_PNG:
-				imagesavealpha($im, true);
-				$this->_gdImageSource = imagepng($im, $file);
-				break;
-			default:
-				trigger_error(__METHOD__.' - Unknown file type', E_USER_NOTICE);
-				return false;
-			}
-			return true;
-		}
-		return false;
-	}
-
-	public function gdThumbnail($filename, $output) {
-		if (function_exists('exif_thumbnail') && file_exists($filename)) {
-			$thumb = exif_thumbnail($filename);
-			if ($thumb) {
-				# $im = imagecreatefromstring($thumb);
-			}
-		}
-		return false;
-	}
-
-	##############################################
-
-	private function gdGetCurrentData() {
-		// Detect what data source we should be using
-		// If output is empty, use the source
-		return (!empty($this->_gdImageOutput)) ? $this->_gdImageOutput : $this->_gdImageSource;
-	}
-
-	##############################################
-
-	public function gdBatchUpload($watermark = false) {
-		// Process a batch upload - take the $_FILES array directly
-		$this->_gdUploaded = false;
-		if (!empty($_FILES)) {
-			foreach ($_FILES as $key => $values) {
-				// Make sure the array isn't empty
-				if ($values['size'] > 0 && !empty($values['tmp_name']) && !empty($values['name'])) {
-					// Pass to gdUpload() for all the real trickery
-					$this->gdUpload($values['tmp_name'], $values['name'], false, $watermark);
-					$this->gdSave($values['name']);
-					$this->gdClear();
-					$this->_gdUploaded[] = $values['name'];
-				}
-			}
-			return ($this->_gdUploaded) ? $this->_gdUploaded : true;
-		}
-		return false;
-	}
-
-	public function gdUpload($file, $name, $rotate = false, $watermark = false, $filter = false) {
-		// First up, lets check the source file actually exists
-		if ($this->gdLoadFile($file)) {
-			// Rotate image
-			if ($rotate) $this->gdRotate($rotate);
-			// Apply filter
-			if ($filter) $this->gdFilter($filter);
-			// Add Watermark image
-			# if ($watermark) $this->gdWatermark($watermark);
-		}
-		return false;
-	}
-
-	##############################################
-	// Edit the image
-
-	public function gdCrop($x, $y, $w, $h) {
-		if ($im = $this->gdGetCurrentData()) {
-			$oh = imagesy($im);
-			$ow = imagesx($im);
-			$h = ($oh < $h) ? $oh : $h;
-			$w = ($ow < $w) ? $ow : $w;
-			$this->_gdImageOutput = imagecreatetruecolor($w, $h);
-			imagecopyresampled($this->_gdImageOutput, $im, 0, 0, $x, $y, $w, $h, $w, $h);
-			imagesavealpha($this->_gdImageOutput, true);
-			$this->_gdImageArray[0] = $w;
-			$this->_gdImageArray[1] = $h;
-		}
-		# trigger_error(sprintf('File cropped to %dx%d@%dx%d', $w, $h, $x, $y), E_USER_NOTICE);
-	}
-
+	/**
+	 * Resize image
+	 * @param int $resize
+	 * @return bool
+	 */
 	private function gdResize($resize) {
 		// Resize the image, while maintaining the proportions
 		$im = $this->gdGetCurrentData();
@@ -211,88 +154,41 @@ class GD {
 		return false;
 	}
 
-	public function gdWatermark($watermark, $y = 'bottom', $x = 'right', $margin = 5) {
-		if ($this->gdLoadWatermark($watermark)) {
-			$im = $this->gdGetCurrentData();
-			// Calculate the image dimensions
-			$src_w  = imagesx($im);
-			$src_h = imagesy($im);
-			$logo_w = imagesx($logo);
-			$logo_h = imagesy($logo);
-			// Work out where we're gonna put the watermark
-			switch ($x) {
-			case 'left': $src_x = $margin; break;
-			case 'right': $src_x = $src_w - ($logo_w+$margin); break;
-			case 'center': $src_x = ($src_w/2) - ($logo_w/2); break;
-				// Allow for exact positioning
-			case is_numeric($x) : $src_x = $x; break;
-			}
-			switch ($y) {
-			case 'top':  $src_y = $margin; break;
-			case 'middle': $src_y = ($src_h/2) - ($logo_h/2); break;
-			case 'bottom': $src_y = $src_h - ($logo_h+$margin); break;
-				// Allow for exact positioning
-			case is_numeric($y) : $src_y = $y; break;
-			}
-			imagesavealpha($img, true);
-			imagesavealpha($logo, true);
-			// Copy the logo onto the main image
-			imagecopy($im, $logo, $src_x, $src_y, 0, 0, $logo_w, $logo_h);
-			$this->_gdImageOutput = $im;
-			return true;
+	/**
+	 * Save modified file
+	 *
+	 * @param string $filename
+	 * @param bool $resize
+	 * @param bool $thumbnail
+	 * @return bool
+	 */
+	public function gdSave($filename, $resize = false, $thumbnail = false) {
+		// Do we need to resize the file before saving?
+		if ($resize || $this->_gdImageMax) {
+			$this->gdResize(($resize) ? $resize : $this->_gdImageMax);
 		}
-		return false;
-	}
-
-	private function gdWatermarkLoad($file) {
-		if (file_exists($file)) {
-			$logo = getimagesize($file);
-			switch ($logo[2]) {
-			case IMAGETYPE_GIF: $src =  imagecreatefromgif($file); break;
-			case IMAGETYPE_JPEG: $src = imagecreatefromjpeg($file); break;
-			case IMAGETYPE_PNG: $src = imagecreatefrompng($file); break;
-			case IMAGETYPE_WBMP: $src = imagecreatefromwbmp($file); break;
-			case IMAGETYPE_XBM: $src = imagecreatefromxbm($file); break;
+		$im = $this->gdGetCurrentData();
+		if ($im) {
+			$file = $this->_gdTargetDir.$filename;
+			imageinterlace($im, true);
+			switch ($this->_gdImageType) {
+			case IMAGETYPE_GIF:
+				$this->_gdImageSource = imagegif($im, $file);
+				break;
+			case IMAGETYPE_JPEG:
+				$this->_gdImageSource = imagejpeg($im, $file, $this->_gdJpegQuality);
+				break;
+			case IMAGETYPE_PNG:
+				imagesavealpha($im, true);
+				$this->_gdImageSource = imagepng($im, $file);
+				break;
 			default:
-				trigger_error(__METHOD__.' - Unknown file type');
+				trigger_error(__METHOD__.' - Unknown file type', E_USER_NOTICE);
 				return false;
 			}
-			imagesavealpha($src, true);
-			$this->_gdWatermark = $src;
-
 			return true;
 		}
 		return false;
 	}
 
-	private function gdRotate($degrees = 90) {
-		// Rotate the image clockwise, preferably by increments of 90 degrees
-		$im = $this->gdGetCurrentData();
-		$degrees = (360 - $degrees);
-
-		$this->_gdImageOutput = imagerotate($im, $degrees, 0);
-
-		if ($degrees != 180) {
-			#$width = $this->_gdImageArray[0];
-			#$height = $this->_gdImageArray[1];
-			$this->_gdImageArray[0] = imagesy($this->_gdImageOutput); //$height;
-			$this->_gdImageArray[1] = imagesx($this->_gdImageOutput); //$width;
-		}
-	}
-
-	private function gdFilter($filter, $arg = '') {
-		// Apply a GD filter to the image, if needed
-		$im = $this->gdGetCurrentData();
-		switch (strtolower($filter)) {
-		case 'negative': $imgfilter = IMG_FILTER_NEGATE; break;
-		case 'greyscale':
-		case 'grayscale': $imgfilter = IMG_FILTER_GRAYSCALE; break;
-		case 'edges':  $imgfilter = IMG_FILTER_EDGEDETECT; break;
-		case 'emboss':  $imgfilter = IMG_FILTER_EMBOSS; break;
-		case 'gaussian': $imgfilter = IMG_FILTER_GAUSSIAN_BLUR; break;
-		case 'sketchy':  $imgfilter = IMG_FILTER_MEAN_REMOVAL; break;
-		}
-		imagefilter($im, $imgfilter, $arg);
-		$this->_gdImageOutput = $im;
-	}
 }
