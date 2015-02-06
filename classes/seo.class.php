03afd32f02b701dbd7f7fbf0be089a4e0c0af75d
Al Brookbanks <al@fathem.com>
2015-02-06 10:54:25 +0000

    Reorganised SEO class

--- classes/seo.class.php
+++ classes/seo.class.php
@@ -16,8 +16,6 @@
  *
  * @author Technocrat
  * @author Al Brookbanks
- * @author Al
- * @version 1.2.0
  * @since 5.0.0
  */
 class SEO {
@@ -111,19 +109,6 @@ class SEO {
 	}
 
 	/**
-	 * Setup the instance (singleton)
-	 *
-	 * @return SEO
-	 */
-	public static function getInstance() {
-		if (!(self::$_instance instanceof self)) {
-			self::$_instance = new self();
-		}
-
-		return self::$_instance;
-	}
-
-	/**
 	 * Add another element to the ignored url segments
 	 *
 	 * @param string $string
@@ -196,48 +181,6 @@ class SEO {
 				}
 		}
 		return $url;
-	}
-
-	/**
-	 * Create item URL
-	 *
-	 * @param string $path
-	 * @param bool $url
-	 */
-	public function getItem($path, $url = false) {
-
-		if (isset($_GET['seo_path'])) unset($_GET['seo_path']);
-
-		if (!empty($path)) {
-			if (($item = $GLOBALS['db']->select('CubeCart_seo_urls', false, array('path' => $path))) !== false) {
-				$item_vars = $this->_getItemVars($item[0]['type'], $item[0]['item_id']);
-				$_GET = (is_array($_GET)) ? array_merge($item_vars, $_GET) : $item_vars;
-				if ($url) {
-					return $GLOBALS['storeURL'].'/index.php?'.http_build_query($_GET);
-				} else {
-					return true;
-				}
-			} else {
-				httpredir('index.php');
-			}
-		} else {
-			httpredir('index.php');
-		}
-	}
-
-	/**
-	 * Get SEO URLs from the DB
-	 *
-	 * @param string $type
-	 * @param string $item_id
-	 * @return string
-	 */
-	public function getdbPath($type, $item_id) {
-		if (($item = $GLOBALS['db']->select('CubeCart_seo_urls', array('path'), array('type' => $type, 'item_id' => $item_id))) !== false) {
-			return $item[0]['path'];
-		} else {
-			return '';
-		}
 	}
 
 	/**
@@ -334,6 +277,62 @@ class SEO {
 		}
 		$safe_path = $this->_safeUrl($path);
 		return (($absolute) ? $GLOBALS['storeURL'] . '/' . $safe_path : $safe_path) . (($extension) ? $this->_extension : '');
+	}
+
+	/**
+	 * Get SEO URLs from the DB
+	 *
+	 * @param string $type
+	 * @param string $item_id
+	 * @return string
+	 */
+	public function getdbPath($type, $item_id) {
+		if (($item = $GLOBALS['db']->select('CubeCart_seo_urls', array('path'), array('type' => $type, 'item_id' => $item_id))) !== false) {
+			return $item[0]['path'];
+		} else {
+			return '';
+		}
+	}
+
+	/**
+	 * Setup the instance (singleton)
+	 *
+	 * @return SEO
+	 */
+	public static function getInstance() {
+		if (!(self::$_instance instanceof self)) {
+			self::$_instance = new self();
+		}
+
+		return self::$_instance;
+	}
+
+	/**
+	 * Create item URL
+	 *
+	 * @param string $path
+	 * @param string $url
+	 * @param bool $url
+	 */
+	public function getItem($path, $url = false) {
+
+		if (isset($_GET['seo_path'])) unset($_GET['seo_path']);
+
+		if (!empty($path)) {
+			if (($item = $GLOBALS['db']->select('CubeCart_seo_urls', false, array('path' => $path))) !== false) {
+				$item_vars = $this->_getItemVars($item[0]['type'], $item[0]['item_id']);
+				$_GET = (is_array($_GET)) ? array_merge($item_vars, $_GET) : $item_vars;
+				if ($url) {
+					return $GLOBALS['storeURL'].'/index.php?'.http_build_query($_GET);
+				} else {
+					return true;
+				}
+			} else {
+				httpredir('index.php');
+			}
+		} else {
+			httpredir('index.php');
+		}
 	}
 
 	/**
@@ -543,7 +542,7 @@ class SEO {
 	}
 
 	/**
-	 * Can we use SEO
+	 * Can we use SEO?
 	 *
 	 * @param string $path
 	 * @return bool/string
@@ -710,6 +709,61 @@ class SEO {
 		return false;
 	}
 
+	//=====[ Private ]=======================================
+
+	/**
+	 * Create .htaccess exists and write if not
+	 *
+	 * @return bool
+	 */
+	private static function _checkModRewrite() {
+		
+		$cache_id = 'seo_check';
+
+		if($GLOBALS['cache']->read($cache_id)) {
+			return false;
+		} else {
+
+			$htaccess_path = CC_ROOT_DIR.'/.htaccess';
+			$htaccess_content = '## File Security
+<FilesMatch "\.(htaccess)$">
+ Order Allow,Deny
+ Deny from all
+</FilesMatch>
+
+#### Apache directory listing rules ####
+DirectoryIndex index.php index.htm index.html
+IndexIgnore *
+
+#### Rewrite rules for SEO functionality ####
+<IfModule mod_rewrite.c>
+  RewriteEngine On
+  RewriteCond %{REQUEST_FILENAME} !-f
+  RewriteCond %{REQUEST_FILENAME} !-d
+  RewriteCond %{REQUEST_URI} !=/favicon.ico
+  RewriteRule ^(.*)\.html?$ index.php?seo_path=$1 [L,QSA]
+</IfModule>';
+
+			if(!file_exists($htaccess_path)) {
+				if(!file_put_contents($htaccess_path, $htaccess_content)) {
+					die('Failed to create .htaccess file for Search Engine Friendly URL\'s. Please create this file in the stores root directory with the content.<textarea style="width: 400px; height: 300px;" readonly>'.$htaccess_content.'</textarea>');
+				}
+				$GLOBALS['cache']->write('1',$cache_id);
+				httpredir();
+			} else {
+				$current_contents = file_get_contents($htaccess_path);
+				if(!strstr($current_contents,'seo_path')) {
+					$htaccess_content = $current_contents."\r\n\r\n".$htaccess_content;
+					if(!file_put_contents($htaccess_path, $htaccess_content)) {
+						die('Failed to update existing .htaccess file for Search Engine Friendly URL\'s. Please edit this file in the stores root directory to have the content.<textarea style="width: 400px; height: 300px;" readonly>'.$htaccess_content.'</textarea>');
+					}
+					$GLOBALS['cache']->write('1',$cache_id);
+					httpredir();
+				}
+			}
+		}
+	}
+
 	/**
 	 * Get the base url
 	 *
@@ -856,53 +910,5 @@ class SEO {
 		$this->_sitemap_xml->setElement('loc',  htmlspecialchars($input['url']), false, false);
 		$this->_sitemap_xml->setElement('lastmod', date('c', $updated), false, false);
 		$this->_sitemap_xml->endElement();
-	}
-
-	private static function _checkModRewrite() {
-		
-		$cache_id = 'seo_check';
-
-		if($GLOBALS['cache']->read($cache_id)) {
-			return false;
-		} else {
-
-			$htaccess_path = CC_ROOT_DIR.'/.htaccess';
-			$htaccess_content = '## File Security
-<FilesMatch "\.(htaccess)$">
- Order Allow,Deny
- Deny from all
-</FilesMatch>
-
-#### Apache directory listing rules ####
-DirectoryIndex index.php index.htm index.html
-IndexIgnore *
-
-#### Rewrite rules for SEO functionality ####
-<IfModule mod_rewrite.c>
-  RewriteEngine On
-  RewriteCond %{REQUEST_FILENAME} !-f
-  RewriteCond %{REQUEST_FILENAME} !-d
-  RewriteCond %{REQUEST_URI} !=/favicon.ico
-  RewriteRule ^(.*)\.html?$ index.php?seo_path=$1 [L,QSA]
-</IfModule>';
-
-			if(!file_exists($htaccess_path)) {
-				if(!file_put_contents($htaccess_path, $htaccess_content)) {
-					die('Failed to create .htaccess file for Search Engine Friendly URL\'s. Please create this file in the stores root directory with the content.<textarea style="width: 400px; height: 300px;" readonly>'.$htaccess_content.'</textarea>');
-				}
-				$GLOBALS['cache']->write('1',$cache_id);
-				httpredir();
-			} else {
-				$current_contents = file_get_contents($htaccess_path);
-				if(!strstr($current_contents,'seo_path')) {
-					$htaccess_content = $current_contents."\r\n\r\n".$htaccess_content;
-					if(!file_put_contents($htaccess_path, $htaccess_content)) {
-						die('Failed to update existing .htaccess file for Search Engine Friendly URL\'s. Please edit this file in the stores root directory to have the content.<textarea style="width: 400px; height: 300px;" readonly>'.$htaccess_content.'</textarea>');
-					}
-					$GLOBALS['cache']->write('1',$cache_id);
-					httpredir();
-				}
-			}
-		}
 	}
 }
