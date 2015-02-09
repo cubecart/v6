dbd4efeb3a5a0527f599ab07ff875de4ca6885e6
Al Brookbanks <al@fathem.com>
2015-02-09 11:12:19 +0000

    Put google analytics into one include

--- skins/foundation/templates/main.php
+++ skins/foundation/templates/main.php
@@ -39,20 +39,7 @@
       {include file='templates/content.recaptcha.head.php'}
       <script src="{$STORE_URL}/skins/{$SKIN_FOLDER}/js/vendor/modernizr.min.js"></script>
       <script src="{$STORE_URL}/skins/{$SKIN_FOLDER}/js/vendor/jquery.js"></script>
-      {if $ANALYTICS}
-      {literal}<script type="text/javascript">
-         var _gaq = _gaq || [];
-         _gaq.push(['_setAccount', '{/literal}{$ANALYTICS}{literal}']);
-         _gaq.push(['_trackPageview']);
-         
-         (function() {
-           var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-           ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-           var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-         })();
-         
-      </script>{/literal}
-      {/if}
+      {include file='templates/element.google_analytics.php'}
       {foreach from=$HEAD_JS item=js}{$js}{/foreach}
    </head>
    <body>
