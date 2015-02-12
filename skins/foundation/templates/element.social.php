857ab7dab5f752ff4b2fc2f95d5e6c84e8a90292
Al Brookbanks <al@fathem.com>
2015-02-12 16:24:12 +0000

    Fixed issue #210

--- skins/foundation/templates/element.social.php
+++ skins/foundation/templates/element.social.php
@@ -13,7 +13,7 @@
    <h3>{$LANG.common.follow_us}</h3>
    <ul class="small-block-grid-4 no-bullet nomarg social-icons text-left">
       {if !empty($CONFIG.twitter)}
-      <li><a href="https://twitter.com/@{$CONFIG.twitter}" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+      <li><a href="https://twitter.com/{$CONFIG.twitter}" title="Twitter"><i class="fa fa-twitter"></i></a></li>
       {/if}
       {if !empty($CONFIG.facebook)}
       <li><a href="https://www.facebook.com/{$CONFIG.facebook}" title="Facebook"><i class="fa fa-facebook"></i></a></li>
@@ -28,7 +28,7 @@
       <li><a href="http://www.youtube.com/user/{$CONFIG.youtube}" title="YouTube"><i class="fa fa-youtube"></i></a></li>
       {/if}
       {if !empty($CONFIG.instagram)}
-      <li><a href="http://www.youtube.com/user/{$CONFIG.instagram}" title="Instagram"><i class="fa fa-instagram"></i></a></li>
+      <li><a href="https://instagram.com/{$CONFIG.instagram}" title="Instagram"><i class="fa fa-instagram"></i></a></li>
       {/if}
       {if !empty($CONFIG.flickr)}
       <li><a href="http://www.flickr.com/photos/{$CONFIG.flickr}" title="Flickr"><i class="fa fa-flickr"></i></a></li>
