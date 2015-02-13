849c3fbd10f5c48c94240d990e9e456389f69c9c
Al Brookbanks <al@fathem.com>
2015-02-13 09:50:20 +0000

    Fixed issue #213

--- classes/catalogue.class.php
+++ classes/catalogue.class.php
@@ -1689,7 +1689,7 @@ class Catalogue {
 			$image_types[] = 'source';
 
 			// Look for images
-			if (($gallery = $GLOBALS['db']->select('`'.$GLOBALS['config']->get('config', 'dbprefix').'CubeCart_image_index` AS `i` INNER JOIN `'.$GLOBALS['config']->get('config', 'dbprefix').'CubeCart_filemanager` AS `f` ON i.file_id = f.file_id', false, 'i.product_id = '.$product_id, array('main_img' => 'DESC')))) {
+			if (($gallery = $GLOBALS['db']->select('`'.$GLOBALS['config']->get('config', 'dbprefix').'CubeCart_image_index` AS `i` INNER JOIN `'.$GLOBALS['config']->get('config', 'dbprefix').'CubeCart_filemanager` AS `f` ON i.file_id = f.file_id', false, 'i.product_id = '.$product_id, 'ORDER BY i.main_img DESC'))) {
 				$duplicates = array();
 				foreach ($gallery as $key => $image) {
 					if (is_array($image_types) && !in_array($image['file_id'], $duplicates)) {
