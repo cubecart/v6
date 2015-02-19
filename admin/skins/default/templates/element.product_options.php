df6e74ee22ff5c1f8ee8644a576e936ba1c56904
Al Brookbanks <al@fathem.com>
2015-02-19 17:36:43 +0000

    Fix for issue #206
    Options have text next to them saying “required” but the html to force
    them to be required has been moved.

--- admin/skins/default/templates/element.product_options.php
+++ admin/skins/default/templates/element.product_options.php
@@ -15,7 +15,7 @@
 <div class="dymanic_options">
    <label for="option_{$option.option_id}" class="return">{$option.option_name}{if $option.price} ({$option.symbol}{$option.price}){/if}{if $option.required} ({$LANG.common.required}){/if}</label><br>
    <span rel="{$product.id}">
-      <select name="inv[{$product.id}][productOptions][{$option.option_id}]" id="option_{$option.option_id}" class="nomarg options_calc" {if $option.required}required{/if}>
+      <select name="inv[{$product.id}][productOptions][{$option.option_id}]" id="option_{$option.option_id}" class="nomarg options_calc">
       <option value="">{$LANG.form.please_select}</option>
       {foreach from=$option.values item=value}
       <option value="{$value.assign_id}"{if $value.selected} selected="selected"{/if} rel="{$value.symbol}{$value.decimal_price}">{$value.value_name}{if $value.price} ({$value.symbol}{$value.price}){/if}</option>
@@ -28,9 +28,9 @@
    <label for="option_{$option.option_id}" class="return">{$option.option_name}{if $option.required}  ({$LANG.common.required}){/if}</label><br>
    <span rel="{$product.id}">
    {if $option.type == '1'}
-   <input type="text" class="text_calc" placeholder="{if $option.price}({$option.symbol}{$option.price}){/if}" name="inv[{$product.id}][productOptions][{$option.option_id}][{$OPT.assign_id}]" id="option_{$option.option_id}" value="{$option.value}" rel="{$option.symbol}{$option.decimal_price}" {if $option.required}required{/if} >
+   <input type="text" class="text_calc" placeholder="{if $option.price}({$option.symbol}{$option.price}){/if}" name="inv[{$product.id}][productOptions][{$option.option_id}][{$OPT.assign_id}]" id="option_{$option.option_id}" value="{$option.value}" rel="{$option.symbol}{$option.decimal_price}">
    {elseif $option.type == '2'}
-   <textarea  class="text_calc" name="inv[{$product.id}][productOptions][{$option.option_id}][{$OPT.assign_id}]" rel="{$option.symbol}{$option.decimal_price}" placeholder="{if $option.price}({$option.symbol}{$option.price}){/if}" id="option_{$option.option_id}" {if $option.required}required{/if}>{$option.value}</textarea>
+   <textarea  class="text_calc" name="inv[{$product.id}][productOptions][{$option.option_id}][{$OPT.assign_id}]" rel="{$option.symbol}{$option.decimal_price}" placeholder="{if $option.price}({$option.symbol}{$option.price}){/if}" id="option_{$option.option_id}">{$option.value}</textarea>
    {/if}
    </span>
 </div>
