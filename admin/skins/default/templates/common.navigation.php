e682cc61c5545b7fabd3ad460db701378ef9ce5e
Al Brookbanks <al@fathem.com>
2015-02-19 12:46:14 +0000

    Added + & - to main nav

--- admin/skins/default/templates/common.navigation.php
+++ admin/skins/default/templates/common.navigation.php
@@ -1,8 +1,8 @@
 {if isset($NAVIGATION)}
 {foreach from=$NAVIGATION item=group}
-<div id="{$group.group}" class="menu">{$group.title}</div>
+<div id="{$group.group}" class="menu">{if $group.visible=="false"}<i class="fa fa-plus-square-o"></i>{else}<i class="fa fa-minus-square-o"></i>{/if} {$group.title}</div>
 {if isset($group.members)}
-<ul id="menu_{$group.group}" class="submenu"{if $group.visible=="false"} style="display: none"{/if}>
+<ul id="menu_{$group.group}" class="submenu{if $group.visible=="false"} hide{/if}">
    {foreach from=$group.members item=nav}
    <li><a href="{$nav.url}" target="{$nav.target}"{if !empty($nav.id)} id="{$nav.id}"{/if}>{$nav.title}</a></li>
    {/foreach}
