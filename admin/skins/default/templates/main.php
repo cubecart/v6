b2a3204ac14a9ef1d1a67446566dcd148ebf5622
Al Brookbanks <al@fathem.com>
2015-02-19 11:39:12 +0000

    Split breadcrumb out into include

--- admin/skins/default/templates/main.php
+++ admin/skins/default/templates/main.php
@@ -35,21 +35,7 @@
             {/foreach}
             {/if}
          </div>
-         <div id="breadcrumbs">
-               <div class="inner">
-                  <ul class="quick_links">
-                  {if $QUICK_TOUR}
-                  <li><a href="#" id="quickTour">{$LANG.dashboard.quick_tour}</a></li>
-                  {/if}
-                  <li><a href="{$HELP_URL}" id="wikihelp" class="colorbox wiki">{$LANG.common.help}</a></li>
-                  <li><a href="index.php" target="_blank">{$LANG.settings.store_status} - {if ($STORE_STATUS)}<span class="store_open">{$LANG.common.open}</span>{else}<span class="store_closed">{$LANG.common.closed}</span>{/if}</a></li>
-                  </ul>
-                  <ul class="location">
-                  <li><i class="fa fa-home"></i> <a href="?">{$LANG.dashboard.title_dashboard}</a></li>
-                  {if isset($CRUMBS)}{foreach from=$CRUMBS item=crumb}<li><i class="fa fa-chevron-right"></i> <a href="{$crumb.url}">{$crumb.title}</a></li>{/foreach}{/if}
-                  </ul>
-               </div>
-            </div>
+         {include file='templates/element.breadcrumb.php'}
          <div id="content">
             <div id="tab_control">
                {if isset($TABS)}
