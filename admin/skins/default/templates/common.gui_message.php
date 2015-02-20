7d40540e891270bb0c9df0dfb2dc385f72f34ea4
Al Brookbanks <al@fathem.com>
2015-02-20 15:33:06 +0000

    Revert "Warning shrunk"
    This reverts commit bb0e4481071f6ee3b4450621cef58cd713e8c3f0.

--- admin/skins/default/templates/common.gui_message.php
+++ admin/skins/default/templates/common.gui_message.php
@@ -12,9 +12,14 @@
 {if isset($GUI_MESSAGE)}
 <div id="gui_message">
   {if isset($GUI_MESSAGE.error)}
-  {foreach from=$GUI_MESSAGE.error item=error}
-  <div class="warnText" title="{$LANG.common.click_to_close}">{$error}</div>
-  {/foreach}
+  <div class="warnText" title="{$LANG.common.click_to_close}">
+	{$LANG.gui_message.errors_detected}
+	<ul>
+		{foreach from=$GUI_MESSAGE.error item=error}
+	  	<li>{$error}</li>
+	  	{/foreach}
+	</ul>
+  </div>
   {/if}
   {if isset($GUI_MESSAGE.notice)}
 	{foreach from=$GUI_MESSAGE.notice item=notice}
