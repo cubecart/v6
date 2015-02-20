bb0e4481071f6ee3b4450621cef58cd713e8c3f0
Al Brookbanks <al@fathem.com>
2015-02-20 15:32:57 +0000

    Warning shrunk

--- admin/skins/default/templates/common.gui_message.php
+++ admin/skins/default/templates/common.gui_message.php
@@ -12,14 +12,9 @@
 {if isset($GUI_MESSAGE)}
 <div id="gui_message">
   {if isset($GUI_MESSAGE.error)}
-  <div class="warnText" title="{$LANG.common.click_to_close}">
-	{$LANG.gui_message.errors_detected}
-	<ul>
-		{foreach from=$GUI_MESSAGE.error item=error}
-	  	<li>{$error}</li>
-	  	{/foreach}
-	</ul>
-  </div>
+  {foreach from=$GUI_MESSAGE.error item=error}
+  <div class="warnText" title="{$LANG.common.click_to_close}">{$error}</div>
+  {/foreach}
   {/if}
   {if isset($GUI_MESSAGE.notice)}
 	{foreach from=$GUI_MESSAGE.notice item=notice}
