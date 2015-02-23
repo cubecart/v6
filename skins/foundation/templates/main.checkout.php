4a93b87c600be2e506cb9ea36369ee2bd7714ff0
Al Brookbanks <al@fathem.com>
2015-02-23 16:00:48 +0000

    Fixed issue #263

--- skins/foundation/templates/main.checkout.php
+++ skins/foundation/templates/main.checkout.php
@@ -85,16 +85,13 @@
                   </div>
                </div>
             </footer>
-            <script src="{$STORE_URL}/skins/{$SKIN_FOLDER}/js/foundation.min.js"></script>
-            <script src="{$STORE_URL}/skins/{$SKIN_FOLDER}/js/vendor/jquery.rating.min.js"></script>
-            <script src="{$STORE_URL}/skins/{$SKIN_FOLDER}/js/vendor/jquery.validate.min.js"></script>
-            <script src="{$STORE_URL}/skins/{$SKIN_FOLDER}/js/vendor/jquery.jscroll.min.js"></script>
-            <script src="{$STORE_URL}/skins/{$SKIN_FOLDER}/js/vendor/jquery.cookie.min.js"></script>
-            <script src="{$STORE_URL}/skins/{$SKIN_FOLDER}/js/cubecart.js"></script>
-            <script src="{$STORE_URL}/skins/{$SKIN_FOLDER}/js/cubecart.validate.js"></script>
+            <script type="text/javascript" src="{$STORE_URL}/skins/{$SKIN_FOLDER}/js/vendor/jquery.rating.min.js"></script>
+            <script type="text/javascript" src="{$STORE_URL}/skins/{$SKIN_FOLDER}/js/vendor/jquery.validate.min.js"></script>
+            <script type="text/javascript" src="{$STORE_URL}/skins/{$SKIN_FOLDER}/js/vendor/jquery.jscroll.min.js"></script>
+            <script type="text/javascript" src="{$STORE_URL}/skins/{$SKIN_FOLDER}/js/vendor/jquery.cookie.min.js"></script>
             {foreach from=$BODY_JS item=js}{$js}{/foreach}
             {foreach from=$JS_SCRIPTS key=k item=script}
-               <script type="text/javascript" src="{$STORE_URL}/{$script|replace:'\\':'/'}"></script>
+            <script type="text/javascript" src="{$STORE_URL}/{$script|replace:'\\':'/'}"></script>
             {/foreach}
             <script>
                $(document).foundation();
