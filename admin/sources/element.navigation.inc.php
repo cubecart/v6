78be4664e9395408416208ed1b995aef94ebc88f
Al Brookbanks <al@fathem.com>
2015-02-25 09:41:20 +0000

    Issue #278 split sales and stats into "Reporting" section

--- admin/sources/element.navigation.inc.php
+++ admin/sources/element.navigation.inc.php
@@ -14,10 +14,11 @@ if (!defined('CC_INI_SET')) die('Access Denied');
 $nav_sections = array(
 	'customers'  => $lang['navigation']['nav_customers'],
 	'inventory'  => $lang['navigation']['nav_inventory'],
+	'reporting'  => $lang['navigation']['nav_reporting'],
 	'filemanager' => $lang['navigation']['nav_file_manager'],
 	'settings'  => $lang['navigation']['nav_settings'],
 	'modules'  => $lang['navigation']['nav_modules'],
-	'advanced'  => $lang['navigation']['nav_advanced'],
+	'advanced'  => $lang['navigation']['nav_advanced']
 );
 
 $nav_items['customers']  = array(
@@ -25,7 +26,7 @@ $nav_items['customers']  = array(
 	$lang['navigation']['nav_orders']   => '?_g=orders',
 	$lang['navigation']['nav_transaction_logs'] => '?_g=orders&node=transactions',
 	$lang['navigation']['nav_newsletters']  => '?_g=customers&node=email',
-	$lang['navigation']['nav_subscribers']  => '?_g=customers&node=subscribers',
+	$lang['navigation']['nav_subscribers']  => '?_g=customers&node=subscribers'
 );
 
 $nav_items['inventory']  = array(
@@ -36,7 +37,12 @@ $nav_items['inventory']  = array(
 	$lang['navigation']['nav_coupons']   => '?_g=products&node=coupons',
 	$lang['navigation']['nav_manufacturers'] => '?_g=products&node=manufacturers',
 	$lang['navigation']['nav_cat_import']  => '?_g=products&node=import',
-	$lang['navigation']['nav_cat_export']  => '?_g=products&node=export',
+	$lang['navigation']['nav_cat_export']  => '?_g=products&node=export'
+);
+
+$nav_items['reporting']  = array(
+	$lang['navigation']['nav_sales_reports'] => '?_g=reports',
+	$lang['navigation']['nav_statistics']  => '?_g=statistics'
 );
 
 $nav_items['filemanager'] = array(
@@ -44,7 +50,7 @@ $nav_items['filemanager'] = array(
 	$lang['navigation']['nav_downloads']  => '?_g=filemanager&mode=digital',
 	$lang['navigation']['nav_images']   => '?_g=filemanager',
 	$lang['navigation']['nav_email_templates'] => '?_g=documents&node=email',
-	$lang['navigation']['nav_contact_form']  => '?_g=documents&node=contact',
+	$lang['navigation']['nav_contact_form']  => '?_g=documents&node=contact'
 );
 
 $nav_items['settings']  = array(
@@ -55,22 +61,20 @@ $nav_items['settings']  = array(
 	$lang['navigation']['nav_currencies']  => '?_g=settings&node=currency',
 	$lang['navigation']['nav_taxes']   => '?_g=settings&node=tax',
 	$lang['navigation']['nav_regions']   => '?_g=settings&node=geo',
-	$lang['navigation']['nav_languages']  => '?_g=settings&node=language',
+	$lang['navigation']['nav_languages']  => '?_g=settings&node=language'
 );
 
 $nav_items['modules']  = array(
 	$lang['navigation']['nav_marketplace']   => array('address' => '?_g=marketplace', 'target' => '_blank', 'id' => 'nav_marketplace', 'icon' => 'fa-external-link'),
-	$lang['navigation']['nav_plugins']   => array('address' => '?_g=plugins', 'id' => 'nav_plugins'),
+	$lang['navigation']['nav_plugins']   => array('address' => '?_g=plugins', 'id' => 'nav_plugins')
 );
 
 $nav_items['advanced']  = array(
-	$lang['navigation']['nav_statistics']  => '?_g=statistics',
-	$lang['navigation']['nav_sales_reports'] => '?_g=reports',
 	$lang['navigation']['nav_access_log']  => '?_g=settings&node=logs',
 	$lang['navigation']['nav_error_log']  => '?_g=settings&node=errorlog',
 	$lang['navigation']['nav_request_log']  => '?_g=settings&node=requestlog',
 	$lang['navigation']['nav_maintenance']  => '?_g=maintenance',
-	$lang['navigation']['nav_server_info']  => '?_g=misc&node=server-info',
+	$lang['navigation']['nav_server_info']  => '?_g=misc&node=server-info'
 );
 
 // Include menu item hooks
