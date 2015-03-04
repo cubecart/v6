47793a31c0598c56d26e21996d4be70d593274d6
Al Brookbanks <al@fathem.com>
2015-03-04 16:18:54 +0000

    Fixed issue #319

---
+++ setup/db/upgrade/6.0.0.sql
@@ -0,0 +1,4 @@
+UPDATE `CubeCart_currency` SET `symbol_left` = '£' WHERE `symbol_left` = '&pound;'; #EOQ
+UPDATE `CubeCart_currency` SET `symbol_left` = '$' WHERE `symbol_left` = '&dollar;'; #EOQ
+UPDATE `CubeCart_currency` SET `symbol_left` = '¥' WHERE `symbol_left` = '&yen;'; #EOQ
+UPDATE `CubeCart_currency` SET `symbol_left` = '€' WHERE `symbol_left` = '&euro;'; #EOQ
