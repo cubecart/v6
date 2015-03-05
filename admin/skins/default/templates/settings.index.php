b72b949fa3d9c7e4db16c73d4871f066f0d7cbb2
Al Brookbanks <al@fathem.com>
2015-03-05 15:29:10 +0000

    Tweak for issue #302

--- admin/skins/default/templates/settings.index.php
+++ admin/skins/default/templates/settings.index.php
@@ -114,7 +114,10 @@
       <h3>{$LANG.settings.title_layout}</h3>
       <fieldset>
          <legend>{$LANG.settings.title_display}</legend>
+         {* This setting has been replaced with layout > products > perpage section of the skins config.xml file
          <div><label for="catalogue_products_per_page">{$LANG.settings.product_per_page}</label><span><input name="config[catalogue_products_per_page]" id="catalogue_products_per_page" class="textbox number" value="{$CONFIG.catalogue_products_per_page}"></span></div>
+         *}
+         <input type="hidden" name="config[catalogue_products_per_page]" value="10">
          <div><label for="default_product_sort">{$LANG.settings.default_product_sort}</label>
             <span>
             <select name="config[product_sort_column]" id="product_sort_column" class="textbox">
