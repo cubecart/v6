14c54246a208b1870ead9b5bb4347f916e8bc5dd
Al Brookbanks <al@fathem.com>
2015-03-24 13:13:08 +0000

    Upgtrade fix

--- setup/db/upgrade/6.0.2.sql
+++ setup/db/upgrade/6.0.2.sql
@@ -1,8 +1,8 @@
 UPDATE `CubeCart_documents` SET `doc_content` = replace(`doc_content`, 'example-orbit', 'bxslider'); #EOQ
 UPDATE `CubeCart_documents` SET `doc_content` = replace(`doc_content`, 'data-orbit=""', ''); #EOQ
 UPDATE `CubeCart_documents` SET `doc_content` = replace(`doc_content`, 'orbit-caption', 'hide'); #EOQ
-UPDATE `CubeCart_documents` SET `doc_content` = replace(`doc_content`, 'slide1.jpg"', ' title="Promotional Message One"'); #EOQ
-UPDATE `CubeCart_documents` SET `doc_content` = replace(`doc_content`, 'slide3.jpg"', ' title="Promotional Message Two"'); #EOQ
+UPDATE `CubeCart_documents` SET `doc_content` = replace(`doc_content`, 'slide1.jpg"', 'slide1.jpg" title="Promotional Message One"'); #EOQ
+UPDATE `CubeCart_documents` SET `doc_content` = replace(`doc_content`, 'slide3.jpg"', 'slide3.jpg" title="Promotional Message Two"'); #EOQ
 ALTER TABLE  `CubeCart_order_summary` ADD UNIQUE (`cart_order_id`); #EOQ
 
 CREATE TABLE IF NOT EXISTS `CubeCart_order_notes` (
