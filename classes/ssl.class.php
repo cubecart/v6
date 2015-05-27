7eb097c6df2cabcd9281eea9f496ee9fb69e7cb6
Al Brookbanks <al@fathem.com>
2015-05-27 09:00:01 +0100

    Tweaks for new SSL class

--- classes/ssl.class.php
+++ classes/ssl.class.php
@@ -41,12 +41,11 @@ class SSL {
 			
 			$ssl_url = $GLOBALS['config']->get('config', 'ssl_url');
 			
-			// Make sure the domain matches for SSL?!
-			if(preg_match('#^'.$ssl_url.'#',$current_url)) {
+			if(preg_match('#^'.$ssl_url.'#',$current_url)) { // Make sure the domain for SSL is expected
 				httpredir($current_url);
-			} else {
-				// Salvage path, query and anchor
+			} else { // If not we try to make it based on what we have
 				$url_parts = parse_url($current_url);
+				
 				$url_parts['path'] = str_replace($GLOBALS['config']->get('config', 'ssl_path'),'/',$url_parts['path']);
 				$ssl_url .= (!empty($url_parts['path'])) ? $url_parts['path'] : '';
 				$ssl_url .= (!empty($url_parts['query'])) ? '?'.$url_parts['query'] : '';
