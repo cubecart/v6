aa2d4aca979ee49237a72843d116a6922fbe08c4
briansandall <brian.sandall@gmail.com>
2015-09-17 10:46:03 -0700

    Fix module admin pages failing to load
    In some cases, such as testing locally on a Windows WAMP stack, modules are recognized but clicking the 'Edit' button for the plugin from 'Manage Plugins' is unable to load the module's admin settings page - the page is simply blank.
    
    Accounting for backslashes in addition to forward slashes in the file path fixes this issue.

--- classes/module.class.php
+++ classes/module.class.php
@@ -417,7 +417,7 @@ class Module {
 	private function _module_data($path = false, $local_name = false) {
 		// Set Module Path
 		if ($path) {
-			$drop = array('/admin', '/classes', '/skin', '/language');
+			$drop = array('/admin', '/classes', '/skin', '/language', '\admin', '\classes', '\skin', '\language');
 			$this->_path = CC_ROOT_DIR.str_replace($drop, '', dirname(str_replace(CC_ROOT_DIR, '', $path)));
 			// Drop trailing slashes
 			if (substr($this->_path, -1) == '/') {
