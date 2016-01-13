8d629cd50f80c1adbc019ec9579476dd28eb9bfc
Al Brookbanks <al@fathem.com>
2016-01-13 10:47:37 +0000

    Implemented #958

--- skins/foundation/templates/content.account.php
+++ skins/foundation/templates/content.account.php
@@ -17,7 +17,7 @@
    <li><a href="{$STORE_URL}/index.php?_a=downloads" title="{$LANG.account.your_downloads}" class="button secondary expand nomarg"><i class="fa fa-download"></i> {$LANG.account.your_downloads}</a></li>
    <li><a href="{$STORE_URL}/index.php?_a=newsletter" title="{$LANG.account.your_subscription}" class="button secondary expand nomarg"><i class="fa fa-envelope"></i> {$LANG.account.your_subscription}</a></li>
    {foreach from=$ACCOUNT_LIST_HOOKS item=list_item}
-   <li><a href="{$list_item.href}" title="{$list_item.title}" class="button secondary expand nomarg">{$list_item.title}</a></li>
+   <li><a href="{$list_item.href}" title="{$list_item.title}" class="button secondary expand nomarg">{if !empty($list_item.fa)}<i class="fa {$list_item.fa}"></i> {/if}{$list_item.title}</a></li>
    {/foreach}
    <li><a href="{$STORE_URL}/index.php?_a=logout" title="{$LANG.account.logout}" class="button secondary expand nomarg"><i class="fa fa-sign-out"></i> {$LANG.account.logout}</a></li>
 </ul>
