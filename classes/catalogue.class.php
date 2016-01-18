8aa96f6cf180ce8e373c52b86be774a80b61ab16
Brian Sandall <brian@linemansequipment.com>
2016-01-18 12:46:28 -0800

    Fixes more massive error log spam
    Produces the following line seemingly once per option value for any not-yet selected option each time the product page loads:
    [Notice] /public_html/classes/catalogue.class.php:446 - Undefined variable: selected

--- classes/catalogue.class.php
+++ classes/catalogue.class.php
@@ -443,7 +443,7 @@ class Catalogue {
 									'absolute_price' => $value['absolute_price']
 								);
 								
-								if($selected[$value['assign_id']]) {
+								if(isset($selected[$value['assign_id']])) {
 									if($value['absolute_price']=='1') {
 										$this->_options_line_price =  $value['option_price'];
 									} else {
