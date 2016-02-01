ba18565f5f38cef98f6f4aa0bcb7969ef91849d4
Al Brookbanks <al@fathem.com>
2016-02-01 16:14:24 +0000

    Revert "Revert "Merge branch '6.1'""
    This reverts commit 2c75e0e7c58ccff72c137bfced2aab664b13d763.

--- setup/db/install/structure.sql
+++ setup/db/install/structure.sql
@@ -873,3 +873,17 @@ CREATE TABLE IF NOT EXISTS `CubeCart_seo_urls` (
   KEY `item_id` (`item_id`),
   KEY `custom` (`custom`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1; #EOQ
+
+CREATE TABLE `CubeCart_email_log` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `content_html` text COLLATE utf8_unicode_ci NOT NULL,
+  `content_text` text COLLATE utf8_unicode_ci NOT NULL,
+  `to` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `from` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `email_content_id` int(11) NOT NULL,
+  `result` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci; #EOQ
+ALTER TABLE `CubeCart_email_log` ADD PRIMARY KEY (`id`); #EOQ
+ALTER TABLE `CubeCart_email_log` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1; #EOQ
