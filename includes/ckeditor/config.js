cfab294687b0ab9dff0a70a9017c42e12cb2c9e0
Al Brookbanks <al@fathem.com>
2016-03-10 16:00:00 +0000

    Fixed #1045

--- includes/ckeditor/config.js
+++ includes/ckeditor/config.js
@@ -29,4 +29,6 @@ CKEDITOR.editorConfig = function( config ) {
 	config.filebrowserBrowseUrl = document.location.pathname+'?_g=filemanager&mode=fck';
 	config.protectedSource.push(/\{.*?}/g);
 	config.extraPlugins = 'showprotected';
+	config.filebrowserWindowHeight  = 500;
+	config.filebrowserWindowWidth  = 650;
 };
