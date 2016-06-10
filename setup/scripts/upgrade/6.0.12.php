de93a3001d6e87acbafba5821cf5fb0eda4e11a0
Al Brookbanks <al@fathem.com>
2016-06-10 09:46:00 +0100

    Tweak for #1122

--- setup/scripts/upgrade/6.0.12.php
+++ setup/scripts/upgrade/6.0.12.php
@@ -1,17 +1,19 @@
 <?php
 $addresses = $db->select('CubeCart_addressbook', array('state', 'address_id'));
-foreach($addresses as $address) {
-	if(strlen($address['state']) == 2 && !is_numeric($address['state'])) {
-		$state = strtoupper($address['state']);
-		$match = $db->select('CubeCart_geo_zone', array('id'), array('abbrev' => $state));
-		if($match && $match[0]['id'] > 0) {
-			$db->update('CubeCart_addressbook', array('state' => $match[0]['id']), array('address_id' => $address['address_id']));
-		}
-	} elseif(!is_numeric($address['state'])) {
-		$state = $address['state'];
-		$match = $db->select('CubeCart_geo_zone', array('id'), array('name' => $state));
-		if($match && $match[0]['id'] > 0) {
-			$db->update('CubeCart_addressbook', array('state' => $match[0]['id']), array('address_id' => $address['address_id']));
-		}
-	} 
+if($addresses) {
+	foreach($addresses as $address) {
+		if(strlen($address['state']) == 2 && !is_numeric($address['state'])) {
+			$state = strtoupper($address['state']);
+			$match = $db->select('CubeCart_geo_zone', array('id'), array('abbrev' => $state));
+			if($match && $match[0]['id'] > 0) {
+				$db->update('CubeCart_addressbook', array('state' => $match[0]['id']), array('address_id' => $address['address_id']));
+			}
+		} elseif(!is_numeric($address['state'])) {
+			$state = $address['state'];
+			$match = $db->select('CubeCart_geo_zone', array('id'), array('name' => $state));
+			if($match && $match[0]['id'] > 0) {
+				$db->update('CubeCart_addressbook', array('state' => $match[0]['id']), array('address_id' => $address['address_id']));
+			}
+		} 
+	}
 }
