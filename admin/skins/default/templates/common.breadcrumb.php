06d956d3bef4863caa90e1849b866e28edd1ccfe
Al Brookbanks <al@fathem.com>
2016-06-13 11:52:40 +0100

    Added more help resources

--- admin/skins/default/templates/common.breadcrumb.php
+++ admin/skins/default/templates/common.breadcrumb.php
@@ -4,7 +4,13 @@
       {if $QUICK_TOUR}
       <li><a href="#" id="quickTour">{$LANG.dashboard.quick_tour}</a></li>
       {/if}
-      <li><a href="{$HELP_URL}" id="wikihelp" class="colorbox wiki">{$LANG.common.help}</a></li>
+      <li id="help_menu"><i class="fa fa-life-ring" aria-hidden="true"></i> <a href="#">{$LANG.common.help}</a>
+         <ul>
+            <li><i class="fa fa-book" aria-hidden="true"></i> <a href="{$HELP_URL}" id="wikihelp" class="colorbox wiki">{$LANG.common.this_page}</a></li>
+            <li><i class="fa fa-comments-o" aria-hidden="true"></i> <a href="https://www.cubecart.com/technical-support" target="_blank">{$LANG.common.tech_support}</a></li>
+            <li><i class="fa fa-users" aria-hidden="true"></i> <a href="https://forums.cubecart.com" target="_blank">CubeCart Forums</a></li>
+         </ul>
+      </li>
       <li><a href="index.php" target="_blank">{$LANG.settings.store_status} - {if ($STORE_STATUS)}<span class="store_open">{$LANG.common.open}</span>{else}<span class="store_closed">{$LANG.common.closed}</span>{/if}</a></li>
       </ul>
       <ul class="location">
