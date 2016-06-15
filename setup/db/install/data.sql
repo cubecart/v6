afff16f143f1c3812a5db74ca34f49d73dbebc6f
Al Brookbanks <al@fathem.com>
2016-06-15 11:14:45 +0100

    Implemented #1057

--- setup/db/install/data.sql
+++ setup/db/install/data.sql
@@ -371,6 +371,7 @@ INSERT INTO `CubeCart_geo_zone` SET `country_id` = 225, `name` = 'West Sussex', 
 INSERT INTO `CubeCart_geo_zone` SET `country_id` = 225, `name` = 'North Yorkshire', `abbrev` = 'YSN'; #EOQ
 INSERT INTO `CubeCart_geo_zone` SET `country_id` = 225, `name` = 'South Yorkshire', `abbrev` = 'YSS'; #EOQ
 INSERT INTO `CubeCart_geo_zone` SET `country_id` = 225, `name` = 'West Yorkshire', `abbrev` = 'YSW'; #EOQ
+INSERT INTO `CubeCart_geo_zone` SET `country_id` = 225, `name` = 'East Yorkshire', `abbrev` = 'ERY'; #EOQ
 
 INSERT INTO `CubeCart_geo_country` (`id`, `iso`, `name`, `iso3`, `numcode`) VALUES (1, 'AF', 'Afghanistan', 'AFG', 004); #EOQ
 INSERT INTO `CubeCart_geo_country` (`id`, `iso`, `name`, `iso3`, `numcode`) VALUES (2, 'AL', 'Albania', 'ALB', 008); #EOQ
--- setup/db/upgrade/6.0.12.sql
+++ setup/db/upgrade/6.0.12.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `CubeCart_category` CHANGE `cat_image` `cat_image` INT(10) NOT NULL; #EOQ
+INSERT INTO `CubeCart_geo_zone` SET `country_id` = 225, `name` = 'East Yorkshire', `abbrev` = 'ERY'; #EOQ
