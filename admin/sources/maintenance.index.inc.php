42cc9db0003f0ee6db2ab16326e4d2ff09394170
Al Brookbanks <al@fathem.com>
2016-09-20 16:21:52 +0100

    Fix for #983 https://github.com/cubecart/v6/issues/983#issuecomment-174425719

--- admin/sources/maintenance.index.inc.php
+++ admin/sources/maintenance.index.inc.php
@@ -165,6 +165,7 @@ if (isset($_GET['upgrade']) && !empty($_GET['upgrade'])) {
 	$contents = false;
 	## Download the version we want
 	$request = new Request('www.cubecart.com', '/download/'.$_GET['upgrade'].'.zip', 80, false, true, 10);
+	$request->setMethod('get');
 	$request->setSSL();
 	$request->setData(array('null'=>0)); // setData needs a value to work
 	$request->setUserAgent('CubeCart');
