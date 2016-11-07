0e91ea90d1fb88d1b225475bf27b30b6ac32cf93
Al Brookbanks <al@brooky.com>
2016-11-07 17:43:12 +0000

    #1334 - Split stripslashes to new private method

--- classes/config.class.php
+++ classes/config.class.php
@@ -220,12 +220,12 @@ class Config {
 	public function set($config_name, $element, $data, $force_write = false) {
 		//Clean up the config array
 		if (is_array($data) && !empty($element)) {
-  			array_walk_recursive($data, function(&$s,$k){return (is_string($s)) ? stripslashes($s) : $s;});
+  			array_walk_recursive($data, function(&$s,$k){return $this->_stripslashes($s);});
   			$data = $this->_json_encode($data);
 		} else if (is_array($data)) {
-  			array_walk_recursive($data, function(&$s,$k){return (is_string($s)) ? stripslashes($s) : $s;});
+  			array_walk_recursive($data, function(&$s,$k){return $this->_stripslashes($s);});
 		} else {
-			$data = (is_string($data)) ? stripslashes($data) : $data;
+			$data = return $this->_stripslashes($data);
 		}
 
 		/**
@@ -263,7 +263,7 @@ class Config {
 	 * @return array
 	 */
 	private function _clean($array) {
-		array_walk_recursive($array, function(&$s,$k){return (is_string($s)) ? stripslashes($s) : $s;});
+		array_walk_recursive($array, function(&$s,$k){return $this->_stripslashes($s);});
 		return $array;
 	}
 
@@ -314,6 +314,13 @@ class Config {
 	private function _json_encode($array) {
 		$this->_pre_enc_config = $array;
 		return base64_encode(json_encode($array));
+	}
+
+	/**
+	 * Strip slashes for strings only
+	 */
+	private function _stripslashes($value) {
+		return is_string($value) ? stripslashes($value) : $value;
 	}
 
 	/**
