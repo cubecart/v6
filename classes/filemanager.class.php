4e5657db96536a31168f7004cd07f5ebc4b028e3
Al Brookbanks <al@brooky.com>
2016-12-06 14:15:04 +0000

    #1379 also allows numeric foreign characters

--- classes/filemanager.class.php
+++ classes/filemanager.class.php
@@ -608,7 +608,7 @@ class FileManager {
 	 * @return string
 	 */
 	private function formatName($name) {
-		return preg_replace('#[^\p{L}\w\.\-\_]#iu', '_', $name);
+		return preg_replace('#[^\p{L}\p{N}\w\.\-\_]#iu', '_', $name);
 	}
 
 	/**
