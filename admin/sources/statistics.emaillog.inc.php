ae23765e0a20c7b8d30833fa4dee5ee9eab7252a
Al Brookbanks <al@brooky.com>
2017-02-01 17:11:37 +0000

    Improvements to email log

--- admin/sources/statistics.emaillog.inc.php
+++ admin/sources/statistics.emaillog.inc.php
@@ -18,7 +18,14 @@ $GLOBALS['gui']->addBreadcrumb($lang['settings']['title_email_log'], currentPage
 
 $per_page = 25;
 $page = (isset($_GET['page'])) ? $_GET['page'] : 1;
-$email_log = $GLOBALS['db']->select('CubeCart_email_log', false, false, array('date' => 'DESC'), $per_page, $page, false);
+$email_logs = $GLOBALS['db']->select('CubeCart_email_log', false, false, array('date' => 'DESC'), $per_page, $page, false);
+if($email_logs!==false) {
+	foreach($email_logs as $row) {
+		$row['to'] = explode(',', $row['to']);
+		$email_log[] = $row;
+	}	
+}
+
 $GLOBALS['smarty']->assign('EMAIL_LOG', $email_log);
 $count = $GLOBALS['db']->count('CubeCart_email_log', 'id');
 $GLOBALS['smarty']->assign('PAGINATION_EMAIL_LOG', $GLOBALS['db']->pagination($count, $per_page, $page, 5, 'page', 'email_log'));
