672377cea479d765f2595c5469eb9ff5db6eb231
Al Brookbanks <al@brooky.com>
2017-08-02 14:08:01 +0100

    Fixed #1696

--- setup/db/upgrade/6.1.9.sql
+++ setup/db/upgrade/6.1.9.sql
@@ -3,3 +3,5 @@ ALTER TABLE `CubeCart_option_assign` CHANGE `option_weight` `option_weight` DECI
 ALTER TABLE `CubeCart_order_summary` CHANGE `weight` `weight` DECIMAL(16,4); #EOQ
 ALTER TABLE `CubeCart_order_summary` CHANGE `basket` `basket` MEDIUMBLOB NULL DEFAULT NULL; #EOQ
 ALTER TABLE `CubeCart_inventory` CHANGE `sale_price` `sale_price` DECIMAL(16,2) NOT NULL DEFAULT '0.00' COMMENT 'Sale Price'; #EOQ
+ALTER TABLE `CubeCart_filemanager` CHANGE `filepath` `filepath` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL; #EOQ
+ALTER TABLE `CubeCart_filemanager` CHANGE `filename` `filename` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL; #EOQ
