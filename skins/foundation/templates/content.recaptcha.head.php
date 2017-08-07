b68df4458815e3f2d41fbcd95f16fbe777486b12
Al Brookbanks <al@brooky.com>
2017-08-07 16:48:20 +0100

    Fixed #1641

--- skins/foundation/templates/content.recaptcha.head.php
+++ skins/foundation/templates/content.recaptcha.head.php
@@ -1,5 +1,5 @@
 {if $RECAPTCHA=='2' || $RECAPTCHA=='3'}
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<script src="https://www.google.com/recaptcha/api.js"></script>
 {/if}
 {if $RECAPTCHA=='3'}
 <script>
