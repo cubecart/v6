cbbae8ef5a583676993b4279771ea220adb87563
Al Brookbanks <al@brooky.com>
2017-09-15 16:33:41 +0100

    Fixed #1726

--- skins/foundation/templates/element.social.php
+++ skins/foundation/templates/element.social.php
@@ -9,6 +9,7 @@
  * Email:  sales@cubecart.com
  * License:  GPL-3.0 https://www.gnu.org/licenses/quick-guide-gplv3.html
  *}
+{if $SOCIAL_LINKS}
 <div class="element-social">
    <h3>{$LANG.common.follow_us}</h3>
    <ul class="small-block-grid-4 no-bullet nomarg social-icons text-left">
@@ -16,4 +17,4 @@
       <li><a href="{$link.url}" title="{$link.name}" target="_blank" rel="noopener noreferrer"><svg class="icon"><use xlink:href="#icon-{$link.icon}"></use></svg></a></li>
       {/foreach}
    </ul>
-</div>
+</div>{/if}
