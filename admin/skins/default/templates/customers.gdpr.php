7a98f6937e5eeac56f2bad97b65e1739cca3081f
Al <al@brooky.com>
2018-03-08 11:20:10 +0000

    #1892 - Added report with hook for other tables

--- admin/skins/default/templates/customers.gdpr.php
+++ admin/skins/default/templates/customers.gdpr.php
@@ -12,5 +12,11 @@
 <form action="{$VAL_SELF}" method="post" enctype="multipart/form-data">
    <div id="general" class="tab_content">
     <h3>{$LANG.search.title_gdpr_report}</h3>
+    <p>{$LANG.search.gdpr_report_desc}</p>
+    <fieldset>
+        <legend>{$LANG.common.search}</legend>
+        <div><label for="email">{$LANG.common.email}</label><span><input type="text" name="email" id="email" value="" class="textbox"></span></div>
+    </fieldset>
+    <input type="submit" value="{$LANG.form.create_report}" name="search">
    </div>
 </form>
--- admin/sources/customers.gdpr.inc.php
+++ admin/sources/customers.gdpr.inc.php
@@ -12,5 +12,38 @@
  */
 if (!defined('CC_INI_SET')) die('Access Denied');
 Admin::getInstance()->permissions('customers', CC_PERM_READ, true);
+if(isset($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    $data = array();
+    // Customer Account
+    $data['customers'] = $GLOBALS['db']->select('CubeCart_customer', false, array('email' => $_POST['email']));
+    // Orders
+    $data['orders'] = $GLOBALS['db']->select('CubeCart_order_summary', false, array('email' => $_POST['email']));
+    // Subscribers
+    $data['subscribers'] = $GLOBALS['db']->select('CubeCart_newsletter_subscriber', false, array('email' => $_POST['email']));
+    // Reviews
+    $data['reviews'] = $GLOBALS['db']->select('CubeCart_reviews', false, array('email' => $_POST['email']));
+    foreach ($GLOBALS['hooks']->load('admin.customer.gdpr.list') as $hook) include $hook;
+    foreach($data as $key => $row) {
+        echo "<h1>$key</h1>";
+        if(is_array($row)) {
+            echo '<table cellspacing="0" cellpadding="3" border="1"><thead><tr>';
+            foreach($row[0] as $key => $value) {
+                echo "<th>".$key."</th>";
+            }
+            echo "</tr></thead><tbody>";
+            foreach($row as $key => $value) {
+                echo "<tr>";
+                foreach($value as $col => $v) {
+                    echo "<td>".$v."</td>";
+                }
+                echo "</tr>";
+            }
+            echo "</tbody></table>";
+        } else {
+            echo "No data";
+        }
+    }
+    exit;
+}
 $GLOBALS['main']->addTabControl($lang['search']['title_gdpr_report'], 'general');
 $page_content = $GLOBALS['smarty']->fetch('templates/customers.gdpr.php');
