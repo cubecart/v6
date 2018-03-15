1afd0b2af54d8df4e12152d4c50f4b35cfd73a95
Al <al@brooky.com>
2018-03-15 14:06:21 +0000

    #1905 - Added literal tags to JSON LD

--- skins/foundation/templates/element.markup.json-ld.php
+++ skins/foundation/templates/element.markup.json-ld.php
@@ -1,6 +1,6 @@
 <script type="application/ld+json">
-{"@context":"http://schema.org","@type":"Organization","legalName":"{$CONFIG.store_name}","url":"{$STORE_URL}","contactPoint":[{"@type":"ContactPoint","url":"{$CONTACT_URL}","contactType":"customer service"}],"logo":"{$STORE_LOGO}"{if $SOCIAL_LINKS},"sameAs":[{foreach from=$SOCIAL_LINKS item=link name=social_links}"{$link.url}"{if !$smarty.foreach.social_links.last},{else}]{/if}{/foreach}{/if}}
+{literal}{"@context":"http://schema.org","@type":"Organization","legalName":"{/literal}{$CONFIG.store_name}{literal}","url":"{/literal}{$STORE_URL}{literal}","contactPoint":[{"@type":"ContactPoint","url":"{/literal}{$CONTACT_URL}{literal}","contactType":"customer service"}],"logo":"{/literal}{$STORE_LOGO}{literal}"{/literal}{if $SOCIAL_LINKS}{literal},"sameAs":[{/literal}{foreach from=$SOCIAL_LINKS item=link name=social_links}{literal}"{/literal}{$link.url}{literal}"{/literal}{if !$smarty.foreach.social_links.last}{literal},{/literal}{else}{literal}]{/literal}{/if}{/foreach}{/if}{literal}}{/literal}
 </script>
 <script type="application/ld+json">
-{"@context":"http://schema.org","@type":"WebSite","name":"{$CONFIG.store_name}","url":"{$STORE_URL}","potentialAction":{"@type":"SearchAction","target":"{$STORE_URL}/search.html?search%5Bkeywords%5D={literal}{search_term}{/literal}&_a=category","query-input":"required name=search_term"}}
+{literal}{"@context":"http://schema.org","@type":"WebSite","name":"{/literal}{$CONFIG.store_name}{literal}","url":"{/literal}{$STORE_URL}{literal}","potentialAction":{"@type":"SearchAction","target":"{/literal}{$STORE_URL}{literal}/search.html?search%5Bkeywords%5D={search_term}&_a=category","query-input":"required name=search_term"}}{/literal}
 </script>
