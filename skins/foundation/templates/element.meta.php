788772632b3d2d015bf927779a5a733ee55eb8f2
Al <al@brooky.com>
2018-04-17 11:26:03 +0100

    #1944 - Added hreflang to head

--- skins/foundation/templates/element.meta.php
+++ skins/foundation/templates/element.meta.php
@@ -8,3 +8,7 @@
 <meta property="og:image" content="{$PRODUCT.thumbnail}">
 <meta property="og:url" content="{$VAL_SELF}">
 {/if}
+<link rel="alternate" hreflang="x-default" href="{$STORE_URL}/index.php?set_language={$CONFIG.default_language}" />
+{foreach from=$LANGUAGES item=language}
+<link rel="alternate" href="{$language.url}" hreflang="{$language.code}">
+{/foreach}
