74930f14c9b88fd6b3deee928076e341433e9b69
Al Brookbanks <al@Als-MacBook-Pro.local>
2018-05-09 16:16:18 +0100

    #2008 - Fixed HEAD_JS

--- skins/foundation/templates/element.js_head.php
+++ skins/foundation/templates/element.js_head.php
@@ -1,6 +1,4 @@
 {assign var=js_head value=[ 'skins/{$SKIN_FOLDER}/js/vendor/modernizr.js',
                             'skins/{$SKIN_FOLDER}/js/vendor/jquery.js']}
-{foreach from=$HEAD_JS item=js}
-    {$js_head[] = $js}
-{/foreach}
+{foreach from=$HEAD_JS item=js}{$js}{/foreach}
 {combine input=$js_head output='cache/js_head.{$SKIN_FOLDER}.js' age='604800' debug=false}
