b13467fa49479c7989c938e7edb0d1f7240b0eec
Al Brookbanks <al@Als-MacBook-Pro.local>
2018-05-09 08:52:38 +0100

    #2006 - Fixed permissions

--- admin/sources/plugins.index.inc.php
+++ admin/sources/plugins.index.inc.php
@@ -11,7 +11,7 @@
  * License:  GPL-3.0 https://www.gnu.org/licenses/quick-guide-gplv3.html
  */
 if (!defined('CC_INI_SET')) die('Access Denied');
-Admin::getInstance()->permissions('settings', CC_PERM_READ, true);
+Admin::getInstance()->permissions('maintenance', CC_PERM_READ, true);
 
 global $lang, $glob;
 
