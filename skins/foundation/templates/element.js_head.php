05f14de22b47f9dffa4dd3db645e7e4a1271b924
Al Brookbanks <al@Als-MacBook-Pro.local>
2018-05-17 08:46:34 +0100

    #2008 - Reorder JS to take advantage of core libs

--- skins/foundation/templates/element.js_head.php
+++ skins/foundation/templates/element.js_head.php
@@ -1,4 +1,4 @@
 {assign var=js_head value=[ 'skins/{$SKIN_FOLDER}/js/vendor/modernizr.js',
                             'skins/{$SKIN_FOLDER}/js/vendor/jquery.js']}
-{foreach from=$HEAD_JS item=js}{$js}{/foreach}
 {combine input=$js_head output='cache/js_head.{$SKIN_FOLDER}.js' age='604800' debug=false}
+{foreach from=$HEAD_JS item=js}{$js}{/foreach}
