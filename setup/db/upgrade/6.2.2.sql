1740f4755eae7d0a680b344eb70ecb8a120b7882
abrookbanks <al@brooky.com>
2018-07-12 10:29:37 +0100

    CubeCart_tax_rates table structure optimization #2105

---
+++ setup/db/upgrade/6.2.2.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `CubeCart_tax_rates` CHANGE `goods` `goods` TINYINT(1)  UNSIGNED  NOT NULL  DEFAULT '0'; #EOQ
+ALTER TABLE `CubeCart_tax_rates` CHANGE `shipping` `shipping` TINYINT(1)  UNSIGNED  NOT NULL  DEFAULT '0'; #EOQ
