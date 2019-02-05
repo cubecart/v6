cb8e03bca616e47c73625b3c33bf579d92b11c3f
abrookbanks <al@brooky.com>
2019-02-05 14:34:42 +0000

    #2092 - Header set X-Content-Type-Options nosniff

--- classes/seo.class.php
+++ classes/seo.class.php
@@ -863,6 +863,7 @@ IndexIgnore *
 
 <ifModule mod_headers.c>
   Header always append X-Frame-Options SAMEORIGIN
+  Header set X-Content-Type-Options nosniff
 </ifModule>
 
 ### Rewrite rules for SEO functionality ###
