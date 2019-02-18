3342e26b4e9c511764ff16d85a15cfa92266f34d
abrookbanks <al@brooky.com>
2019-02-18 11:27:24 +0000

    #2241 - Fixed SQL for sale mode "2" and improved count method

--- classes/catalogue.class.php
+++ classes/catalogue.class.php
@@ -1985,9 +1985,9 @@ class Catalogue
                         $order_string = 'ORDER BY `'.key($order).'` '.current($order);
                     }
                 }
+                $where2 = $this->outOfStockWhere(false, 'I', true);
+                $whereString = 'IF (G.sale_price IS NULL, I.sale_price, G.sale_price) > 0'.$where2;
                 if ($GLOBALS['config']->get('config', 'catalogue_sale_mode') == '1') {
-                    $where2 = $this->outOfStockWhere(false, 'I', true);
-                    $whereString = 'IF (G.sale_price IS NULL, I.sale_price, G.sale_price) > 0'.$where2;
                     $query = sprintf("SELECT I.* FROM %1\$sCubeCart_inventory AS I LEFT JOIN (SELECT product_id, MAX(price) as price, MAX(sale_price) as sale_price FROM %1\$sCubeCart_pricing_group $group_id GROUP BY product_id) as G ON G.product_id = I.product_id WHERE I.product_id IN (SELECT product_id FROM `%1\$sCubeCart_category_index` as CI INNER JOIN %1\$sCubeCart_category as C where CI.cat_id = C.cat_id AND C.status = 1) AND I.status = 1 AND %2\$s %3\$s %4\$s", $GLOBALS['config']->get('config', 'dbprefix'), $whereString, $order_string, $limit);
                 } elseif ($GLOBALS['config']->get('config', 'catalogue_sale_mode') == '2') {
                     $decimal_percent = $GLOBALS['config']->get('config', 'catalogue_sale_percentage')/100;
@@ -1999,9 +1999,9 @@ class Catalogue
                     include $hook;
                 }
                 if (($sale = $GLOBALS['db']->query($query)) !== false) {
-                    $q2 = sprintf("SELECT I.* FROM %1\$sCubeCart_inventory AS I LEFT JOIN (SELECT product_id, MAX(price) as price, MAX(sale_price) as sale_price FROM %1\$sCubeCart_pricing_group $group_id GROUP BY product_id) as G ON G.product_id = I.product_id WHERE I.product_id IN (SELECT product_id FROM `%1\$sCubeCart_category_index` as CI INNER JOIN %1\$sCubeCart_category as C where CI.cat_id = C.cat_id AND C.status = 1) AND I.status = 1 AND %2\$s %3\$s", $GLOBALS['config']->get('config', 'dbprefix'), $whereString, $order_string);
-                    $count = $GLOBALS['db']->query($q2);
-                    $this->_category_count  = (int)count($count);
+                    $q2 = sprintf("SELECT SQL_CALC_FOUND_ROWS I.* FROM %1\$sCubeCart_inventory AS I LEFT JOIN (SELECT product_id, MAX(price) as price, MAX(sale_price) as sale_price FROM %1\$sCubeCart_pricing_group $group_id GROUP BY product_id) as G ON G.product_id = I.product_id WHERE I.product_id IN (SELECT product_id FROM `%1\$sCubeCart_category_index` as CI INNER JOIN %1\$sCubeCart_category as C where CI.cat_id = C.cat_id AND C.status = 1) AND I.status = 1 AND %2\$s %3\$s", $GLOBALS['config']->get('config', 'dbprefix'), $whereString, $order_string);
+                    $count = $GLOBALS['db']->misc('SELECT FOUND_ROWS() as Count', false);
+                    $this->_category_count = (int)$count[0]['Count'];
                     $this->_category_products = $sale;
                     foreach ($GLOBALS['hooks']->load('class.catalogue.search_catalogue.sale_items.post') as $hook) {
                         include $hook;
