311930408e1e8bee29b085c811443014b0faa524
abrookbanks <al@brooky.com>
2019-04-18 12:38:59 +0100

    #2284 - Corrected protected source regex

--- includes/ckeditor/config.js
+++ includes/ckeditor/config.js
@@ -5,7 +5,7 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	config.filebrowserBrowseUrl = document.location.pathname+'?_g=filemanager&mode=fck';
-	config.protectedSource.push(/{[\s\S]*?}/g);
+	config.protectedSource.push(/{\S*?.*\S}/g);
 	config.extraPlugins = 'showprotected';
 	config.filebrowserWindowHeight  = 500;
 	config.filebrowserWindowWidth  = 650;
