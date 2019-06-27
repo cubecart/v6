2d984ccdc716fe9e65858eeffbb8ccf7e5421ff3
Al <al@brooky.com>
2019-06-27 15:06:38 +0100

    #2303 - added digital to specs

--- skins/foundation/templates/content.product.php
+++ skins/foundation/templates/content.product.php
@@ -92,6 +92,12 @@
                      <td>{floatval($PRODUCT.product_depth)}{if $PRODUCT.dimension_unit=='in'}&#8243;{else}{$PRODUCT.dimension_unit}{/if}</td>
                   </tr>
                   {/if}
+                  {if $PRODUCT.digital > 0}
+                  <tr>
+                     <td>{$LANG.catalogue.product_type_digital}</td>
+                     <td>{$LANG.common.download}</td>
+                  </tr>
+                  {/if}
                </tbody>
             </table>
          </div>
