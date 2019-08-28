0b9c5263579e27619c19243531b1f38dd43cf887
Al <al@brooky.com>
2019-08-28 13:54:59 +0100

    #2365 - Swapped back to protectedSource plugin

--- includes/ckeditor/config.js
+++ includes/ckeditor/config.js
@@ -4,27 +4,9 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	config.toolbarGroups = [
-		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-		{ name: 'forms', groups: [ 'forms' ] },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-		{ name: 'links', groups: [ 'links' ] },
-		{ name: 'insert', groups: [ 'insert' ] },
-		'/',
-		{ name: 'styles', groups: [ 'styles' ] },
-		{ name: 'colors', groups: [ 'colors' ] },
-		{ name: 'tools', groups: [ 'tools' ] },
-		{ name: 'others', groups: [ 'others' ] },
-		{ name: 'about', groups: [ 'about' ] }
-	];
-
-	config.removeButtons = 'Save,Print,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,CopyFormatting,Language,About,Flash,Iframe';
 	config.filebrowserBrowseUrl = document.location.pathname+'?_g=filemanager&mode=fck';
-	config.extraPlugins = 'placeholder';
+	config.protectedSource.push(/{\S*?.*?\S}/g);
+	config.extraPlugins = 'showprotected';
 	config.filebrowserWindowHeight  = 500;
 	config.filebrowserWindowWidth  = 650;
 	config.allowedContent = true;
