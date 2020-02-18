6af053eed7c0231ebd29ab1f90f59ac1bc01d020
Al <al@brooky.com>
2020-02-18 15:19:25 +0000

    Joyride Improvements

--- admin/skins/default/templates/element.welcome_tour.php
+++ admin/skins/default/templates/element.welcome_tour.php
@@ -26,21 +26,25 @@
 			<h2>3. {$LANG.navigation.nav_products}</h2>
 			<p>{$LANG.dashboard.tour_3}</p>
 		</li>
-		<li data-id="nav_marketplace">
-			<h2>4. {$LANG.navigation.nav_marketplace}</h2>
-			<p>{$LANG.dashboard.tour_4}</p>
-		</li>
 		<li data-id="nav_plugins">
-			<h2>5. {$LANG.navigation.nav_plugins}</h2>
+			<h2>4. {$LANG.navigation.nav_plugins}</h2>
 			<p>{$LANG.dashboard.tour_5}</p>
+		</li>
+		<li data-id="nav_marketplace">
+			<h2>5. {$LANG.navigation.nav_marketplace}</h2>
+			<p>{$LANG.dashboard.tour_4}</p>
 		</li>
 		<li data-id="nav_docs">
 		<h2>6. {$LANG.dashboard.tour_6_title}</h2>
 			<p>{$LANG.dashboard.tour_6}</p>
 		</li>
-		<li data-id="clear_cache_master">
+		<li data-id="clear_cache_master" class="cache">
 			<h2>7. {$LANG.dashboard.tour_7_title}</h2>
 			<p>{$LANG.dashboard.tour_7}</p>
+		</li>
+		<li data-id="help_menu" class="help">
+			<h2>8. {$LANG.dashboard.tour_8_title}</h2>
+			<p>{$LANG.dashboard.tour_8}</p>
 		</li>
 		<li class="joyride_tour_end" data-button="{$LANG.common.close}">
 			<h2>{$LANG.common.thats_it}</h2>
