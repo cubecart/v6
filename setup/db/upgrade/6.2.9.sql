d387d5081d4366adfff0c839ecfd796aeaf64ccc
Al <al@brooky.com>
2020-02-19 16:31:42 +0000

    #2452 -  Filter duplicate from cookie_content table

--- setup/db/upgrade/6.2.9.sql
+++ setup/db/upgrade/6.2.9.sql
@@ -1,2 +1,8 @@
-ALTER TABLE `CubeCart_cookie_consent` ADD `log_hash` VARCHAR(32) NOT NULL AFTER `time`, ADD `url_shown` VARCHAR(255) NOT NULL AFTER `log_hash`; #EOQ
+ALTER TABLE `CubeCart_cookie_consent` ADD `log_hash` VARCHAR(32) NOT NULL; #EOQ
+ALTER TABLE `CubeCart_cookie_consent` ADD `url_shown` VARCHAR(255) NOT NULL; #EOQ
+UPDATE `CubeCart_cookie_consent` SET `log_hash` = MD5(`log`); #EOQ
 ALTER TABLE `CubeCart_cookie_consent` ADD INDEX(`log_hash`); #EOQ
+CREATE TEMPORARY TABLE `tmp_cookie_table` (SELECT * FROM `CubeCart_cookie_consent` GROUP BY `log`,`session_id`); #EOQ
+TRUNCATE `CubeCart_cookie_consent`; #EOQ
+INSERT INTO `CubeCart_cookie_consent` (SELECT * FROM `tmp_cookie_table`); #EOQ
+DROP TEMPORARY TABLE `tmp_cookie_table`; #EOQ
