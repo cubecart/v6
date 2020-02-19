28556ded5ff5ef78c2143d897b779f041a5733c3
Al <al@brooky.com>
2020-02-19 16:32:30 +0000

    #2452 - Optimise by grouping on new index

--- setup/db/upgrade/6.2.9.sql
+++ setup/db/upgrade/6.2.9.sql
@@ -2,7 +2,7 @@ ALTER TABLE `CubeCart_cookie_consent` ADD `log_hash` VARCHAR(32) NOT NULL; #EOQ
 ALTER TABLE `CubeCart_cookie_consent` ADD `url_shown` VARCHAR(255) NOT NULL; #EOQ
 UPDATE `CubeCart_cookie_consent` SET `log_hash` = MD5(`log`); #EOQ
 ALTER TABLE `CubeCart_cookie_consent` ADD INDEX(`log_hash`); #EOQ
-CREATE TEMPORARY TABLE `tmp_cookie_table` (SELECT * FROM `CubeCart_cookie_consent` GROUP BY `log`,`session_id`); #EOQ
+CREATE TEMPORARY TABLE `tmp_cookie_table` (SELECT * FROM `CubeCart_cookie_consent` GROUP BY `log_hash`,`session_id`); #EOQ
 TRUNCATE `CubeCart_cookie_consent`; #EOQ
 INSERT INTO `CubeCart_cookie_consent` (SELECT * FROM `tmp_cookie_table`); #EOQ
 DROP TEMPORARY TABLE `tmp_cookie_table`; #EOQ
