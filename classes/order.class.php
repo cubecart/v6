749d2b710f91d6775125b4792f236053667531a7
Al Brookbanks <al@brooky.com>
2020-09-10 15:15:05 +0100

    #2598 - Traditional always valid

--- classes/order.class.php
+++ classes/order.class.php
@@ -919,7 +919,7 @@ class Order
     public static function validOrderId($order_id, $traditional = false)
     {
         $oid_mode = $GLOBALS['config']->get('config', 'oid_mode');
-        if (($oid_mode=='t' || !$oid_mode || $traditional) && preg_match('#^[0-9]{6}-[0-9]{6}-[0-9]{4}$#i', $order_id)) {
+        if (preg_match('#^[0-9]{6}-[0-9]{6}-[0-9]{4}$#i', $order_id)) {
             return true;
         } elseif ($oid_mode=='i' && (ctype_digit($order_id) || preg_match('/[\w-\_]/', $order_id))) {
             return true;
