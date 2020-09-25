369bb8def16847f2eb1c706c72538d63ee8f4c0d
Al Brookbanks <al@brooky.com>
2020-09-25 15:14:49 +0100

    #1164 - Path fix on upgrade

--- setup/scripts/upgrade/6.4.0.php
+++ setup/scripts/upgrade/6.4.0.php
@@ -4,7 +4,7 @@ $config = json_decode(base64_decode($config_string[0]['array']), true);
 $added_config = array('seo_ext' => '.html');
 $db->update('CubeCart_config', array('array' => base64_encode(json_encode(array_merge($config, $added_config)))), array('name'=>'config'));
 
-$htaccess_path = CC_ROOT_DIR.'.htaccess';
+$htaccess_path = CC_ROOT_DIR.'/.htaccess';
 $content = file_get_contents($htaccess_path);
 $content = str_replace('^(.*)\.html?$', '^(.*)?$', $content);
 file_put_contents($htaccess_path, $content);
