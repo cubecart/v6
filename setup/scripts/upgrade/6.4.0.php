2cb75fb377cd31a7bdd9a2fefedccc6659777217
Al Brookbanks <al@brooky.com>
2020-09-25 17:01:23 +0100

    #1164 - Improved find/replace

--- setup/scripts/upgrade/6.4.0.php
+++ setup/scripts/upgrade/6.4.0.php
@@ -4,7 +4,8 @@ $config = json_decode(base64_decode($config_string[0]['array']), true);
 $added_config = array('seo_ext' => '.html');
 $db->update('CubeCart_config', array('array' => base64_encode(json_encode(array_merge($config, $added_config)))), array('name'=>'config'));
 
-$htaccess_path = CC_ROOT_DIR.'/.htaccess';
-$content = file_get_contents($htaccess_path);
-$content = str_replace('^(.*)\.html?', '^(.*)?', $content);
-file_put_contents($htaccess_path, $content);
+$filename = CC_ROOT_DIR.'/.htaccess';
+$content = file_get_contents($filename);
+$content_chunks = explode('^(.*)\.html?', $content);
+$content = implode('^(.*)?', $content_chunks);
+file_put_contents($filename, $content);
