d7be54048aa6584304b49bba3ef4f9f20b6e1ccc
Al Brookbanks <al@brooky.com>
2020-09-29 08:18:03 +0100

    #2612 - Added _download to _ignored array

--- classes/seo.class.php
+++ classes/seo.class.php
@@ -50,7 +50,7 @@ class SEO
      * @var array of strings
      */
     private $_ignored   = array(
-        'account', 'addressbook', 'basket', 'checkout', 'complete', 'confirm', 'downloads', 'gateway', 'logout', 'profile', 'receipt', 'recover', 'recovery', 'remote', 'vieworder', 'plugin', 'unsubscribe'
+        'account', 'addressbook', 'basket', 'checkout', 'complete', 'confirm', 'download', 'downloads', 'gateway', 'logout', 'profile', 'receipt', 'recover', 'recovery', 'remote', 'vieworder', 'plugin', 'unsubscribe'
     );
     /**
      * Rewrite URL Absolute?
