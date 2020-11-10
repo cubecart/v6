5663c750d8e0fb34b86770aa1ad3c244a82670bf
Al Brookbanks <al@brooky.com>
2020-11-10 11:19:12 +0000

    Moved config update to correct file

--- setup/scripts/upgrade/6.2.10.php
+++
@@ -1,7 +0,0 @@
-<?php
-$config_string = $db->select('CubeCart_config', array('array'), array('name' => 'gift_certs'));
-$config = json_decode(base64_decode($config_string[0]['array']), true);
-if(is_numeric($config['product_code'])) {
-    $added_config = array('product_code' => 'GC'.$config['product_code']);
-    $db->update('CubeCart_config', array('array' => base64_encode(json_encode(array_merge($config, $added_config)))), array('name'=>'gift_certs'));
-}
