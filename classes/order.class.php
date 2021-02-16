1e1383fc334e6bdb9afb6451a1376f811d4612c7
Al Brookbanks <al@brooky.com>
2021-02-16 11:02:01 +0000

    #2781 - IS NULL

--- classes/order.class.php
+++ classes/order.class.php
@@ -898,7 +898,7 @@ class Order
                     $GLOBALS['db']->misc("UPDATE `".$GLOBALS['config']->get('config', 'dbprefix')."CubeCart_order_summary` SET `custom_oid` = ".$concat);
                 } else {
                     $column = $GLOBALS['config']->get('config', 'oid_mode')=='i' ? 'id' : 'cart_order_id';
-                    $GLOBALS['db']->misc("UPDATE `".$GLOBALS['config']->get('config', 'dbprefix')."CubeCart_order_summary` SET `custom_oid` = `$column` WHERE `custom_oid` = '' OR `custom_oid` = null");
+                    $GLOBALS['db']->misc("UPDATE `".$GLOBALS['config']->get('config', 'dbprefix')."CubeCart_order_summary` SET `custom_oid` = `$column` WHERE `custom_oid` = '' OR `custom_oid` IS NULL");
                 }
                 $GLOBALS['db']->misc("DROP TRIGGER IF EXISTS `custom_oid`");
                 $oid_col = 'custom_oid';
