18665d4f3ab51f3d227919e82e4ad24a90aaabe1
Al Brookbanks <al@brooky.com>
2021-05-20 08:21:22 +0100

    #2869 - featured product style tweaks

--- skins/foundation/templates/box.featured.php
+++ skins/foundation/templates/box.featured.php
@@ -12,14 +12,16 @@
 {if $featured}
 <div class="panel" id="box-featured">
    <h3>{$LANG.catalogue.title_feature}</h3>
-   <a class="th" href="{$featured.url}" title="{$featured.name}">
-   <img src="{$featured.image}" alt="{$featured.name}">
-   </a>
+   <div class="text-center">
+      <a class="th" href="{$featured.url}" title="{$featured.name}">
+         <img src="{$featured.image}" alt="{$featured.name}">
+      </a>
+   </div>
    <h4><a href="{$featured.url}" title="{$featured.name}">{$featured.name}</a></h4>
    {if $featured.ctrl_sale}
    <span class="old_price">{$featured.price}</span> <span class="sale_price">{$featured.sale_price}</span>
    {else}
-   {$featured.price}
+   <span>{$featured.price}</span>
    {/if}
 </div>
 {/if}
