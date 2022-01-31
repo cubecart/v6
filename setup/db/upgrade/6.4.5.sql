d6e73c0dea8084f101757c64a3e43e36a962db4b
Al Brookbanks <al@brooky.com>
2022-01-31 10:36:53 +0000

    #2955 - Updated Laois

--- setup/db/upgrade/6.4.5.sql
+++ setup/db/upgrade/6.4.5.sql
@@ -1,1 +1,2 @@
-ALTER TABLE `CubeCart_filemanager` CHANGE `filesize` `filesize` BIGINT UNSIGNED NOT NULL;
+ALTER TABLE `CubeCart_filemanager` CHANGE `filesize` `filesize` BIGINT UNSIGNED NOT NULL; #EOQ
+UPDATE `CubeCart_geo_zone` SET `name` = 'Laois' WHERE `name` LIKE 'Laoighis'; #EOQ
