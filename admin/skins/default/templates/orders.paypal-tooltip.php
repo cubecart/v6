dea3fec5f7dcd0e09b4b9a50102c4c4c429d6fce
Al Brookbanks <al@brooky.com>
2022-02-04 10:36:01 +0000

    #2954 - Tooltip tweak

--- admin/skins/default/templates/orders.paypal-tooltip.php
+++ admin/skins/default/templates/orders.paypal-tooltip.php
@@ -3,8 +3,14 @@
 .ppcp-container {
 display: flex;
 }
-.ppcp-container a {
-color: #139bd7
+.ppcp-container a, .fa-times {
+color: #fff;
+}
+a.pp_go  {
+    text-decoration: underline;
+}
+a.pp_go:hover  {
+    text-decoration: none;
 }
 .flex-child {
 flex: 1;
@@ -24,10 +30,19 @@ font-size: 14px;
 }
 </style>
 <div class="ppcp-container">
-<div class="flex-child monogram"></div>
-<div class="flex-child message">
-Did you know? PayPal Standard has now been superseded by PayPal Commerce.<br><a href="https://www.cubecart.com/extensions/plugins/paypal-commerce" target="_blank">Find out more and upgrade today &raquo;</a>
-</div>
-
+    <div class="flex-child monogram"></div>
+    <div class="flex-child message">
+        <a href="#" onmouseover="$('#pp_expand').slideDown()">Did you know? PayPal Standard has now been superseded by PayPal Commerce.</a></a>
+        <div style="display: none;" id="pp_expand">
+            <ul>
+                <li>Higher conversions with PayPal Express Checkout in context flow.</li>
+                <li>Take secure card payments with advanced 3D secure compliance.</li>
+                <li>Attract more sales with PayPal's BNPL product, Pay Later.</li>
+                <li>Customizable checkout flow with the most relevant payment methods presented to customers (PayPal, Pay Later, Card Brands and Local Payment Methods).</li>
+            </ul>
+            <a href="#" style="float: right" onclick="$('#pp_expand').slideUp()" title="Close"><i class="fa fa-times" aria-hidden="true"></i></a>
+            <a href="https://www.cubecart.com/extensions/plugins/paypal-commerce" class="pp_go" target="_blank">Learn more and upgrade today</a>
+        </div>
+    </div>
 </div>
 {/if}
