9f97a51ce7ec6a1e18dd5e5dd2f9d8d21063f72d
Al Brookbanks <al@brooky.com>
2022-05-24 10:59:18 +0100

    #3004 - 1G fix

--- classes/gd.class.php
+++ classes/gd.class.php
@@ -235,8 +235,12 @@ class GD
         $this->_abort = false;
         
         $memoryNeeded = round(($this->_gdImageData[0] * $this->_gdImageData[1] * $this->_gdImageData['bits'] * $this->_gdImageData['channels'] / 8 + Pow(2, 16)) * 1.65);
-        if (function_exists('memory_get_usage') && memory_get_usage() + $memoryNeeded > (integer) ini_get('memory_limit') * pow(1024, 2)) {
-            $new_memory_limit = (integer) ini_get('memory_limit') + ceil(((memory_get_usage() + $memoryNeeded) - (integer) ini_get('memory_limit') * pow(1024, 2)) / pow(1024, 2)) . 'M';
+        
+        $memLimit = ini_get('memory_limit');
+        $memLimit = substr($memLimit, -1) == 'G' ? (int)$memLimit * 1024 : (int)$memLimit;
+    
+        if (function_exists('memory_get_usage') && memory_get_usage() + $memoryNeeded > $memLimit * pow(1024, 2)) {
+            $new_memory_limit = $memLimit + ceil(((memory_get_usage() + $memoryNeeded) - $memLimit * pow(1024, 2)) / pow(1024, 2)) . 'M';
             // ini_set may be a disabled function
             if (!function_exists('ini_set')) {
                 $this->_abort = true;
