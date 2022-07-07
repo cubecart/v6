d1d00b51440906a8bd3078e93b931c4a635761a7
Al Brookbanks <al@brooky.com>
2022-07-07 08:50:27 +0100

    #2963 - Split pagination to template files

--- skins/foundation/templates/content.category.php
+++ skins/foundation/templates/content.category.php
@@ -196,30 +196,7 @@
       {/if}
       {/foreach}
    </ul>
-   {* Uncomment for traditional pagination *}
-   {*
-   <div class="row">
-      <div class="small-12 large-8 columns">
-         {$PAGINATION}
-      </div>
-      <div class="large-4 columns show-for-medium-up">
-         <dl>
-            <dd>
-               <select class="url_select">
-               {foreach from=$PAGE_SPLITS item=page_split}
-               <option value="{$page_split.url}"{if $page_split.selected} selected{/if}>{$LANG.common.show} {$page_split.amount} {$LANG.settings.product_per_page|lower}</option>
-               {/foreach}
-               </select>
-            </dd>
-         </dl>
-      </div>
-   </div>
-   *}
-   <div class="hide" id="ccScrollCat">{$category.cat_id}</div>
-   {if $page!=='all' && ($page < $total)}
-   {$params[$var_name] = $page + 1}
-   {* Add "hide-for-medium-up" to the class attribute to not display the more button *}
-   <a href="{$current}{http_build_query($params)}{$anchor}" data-next-page="{$params[$var_name]}" data-cat="{$category.cat_id}" class="button tiny expand ccScroll-next">{$LANG.common.more} <svg class="icon"><use xlink:href="#icon-angle-down"></use></svg></a>
-   {/if}
-   <div class="text-center hide" id="loading"><svg class="icon-x3"><use xlink:href="#icon-spinner"></use></svg></div>
+   {* Choose between "traditional" 123 pagination or "more" button to load next page inline *}
+   {* include file='templates/element.category.pagination.traditional.php' *}
+   {include file='templates/element.category.pagination.more.php'}
 </div>
---
+++ skins/foundation/templates/element.category.pagination.more.php
@@ -0,0 +1,7 @@
+<div class="hide" id="ccScrollCat">{$category.cat_id}</div>
+{if $page!=='all' && ($page < $total)}
+{$params[$var_name] = $page + 1}
+{* Add "hide-for-medium-up" to the class attribute to not display the more button *}
+<a href="{$current}{http_build_query($params)}{$anchor}" data-next-page="{$params[$var_name]}" data-cat="{$category.cat_id}" class="button tiny expand ccScroll-next">{$LANG.common.more} <svg class="icon"><use xlink:href="#icon-angle-down"></use></svg></a>
+{/if}
+<div class="text-center hide" id="loading"><svg class="icon-x3"><use xlink:href="#icon-spinner"></use></svg></div>
