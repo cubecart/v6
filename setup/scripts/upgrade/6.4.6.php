7624d568e6dcd770cf7c4a4a9e563e5fa2b3df1a
Al Brookbanks <al@brooky.com>
2022-08-08 16:42:16 +0100

    Upgrade Script Fix

--- setup/scripts/upgrade/6.4.6.php
+++ setup/scripts/upgrade/6.4.6.php
@@ -1,9 +1,7 @@
 <?php
 $config_string = $db->select('CubeCart_config', array('array'), array('name' => 'config'));
 $config = json_decode(base64_decode($config_string[0]['array']), true);
-$config['time_format'] = 'j M Y, H:i';
-$config['fuzzy_time_format'] = 'H:i';
-$config['dispatch_date_format'] = 'M d Y';
+$added_config = array('time_format' => 'j M Y, H:i', 'fuzzy_time_format' => 'H:i', 'dispatch_date_format' => 'M d Y');
 $db->update('CubeCart_config', array('array' => base64_encode(json_encode(array_merge($config, $added_config)))), array('name'=>'config'));
 # This code is duplicated in admin/sources/categories.inc.php
 function updateCatsWithHierPosition($cat_id = 0, $position = 0){
