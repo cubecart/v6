4cd302be20ffcf40ca97806f62787a769fc74e97
Al Brookbanks <al@brooky.com>
2022-11-15 08:12:49 +0000

    #3075 - Missing Span Tag

--- skins/foundation/templates/content.certificates.php
+++ skins/foundation/templates/content.certificates.php
@@ -25,7 +25,7 @@
 	{if in_array($GC.delivery, array(1,3))}
 	<div class="row"><div class="small-12 large-8 columns" id="gc-method-e"><label for="gc-email">{$LANG.catalogue.recipient_email}</label><input type="text" name="gc[email]" id="gc-email" placeholder="{$LANG.catalogue.recipient_email} {$LANG.form.required}" value="{$POST.email}"></div></div>
 	{/if}
-	<div class="row"><div class="small-12 large-8 columns"><label for="gc-message">{$LANG.common.message} {$LANG.common.optional}</label><span><textarea name="gc[message]" id="gc-message">{$POST.message}</textarea></div></div>
+	<div class="row"><div class="small-12 large-8 columns"><label for="gc-message">{$LANG.common.message} {$LANG.common.optional}</label><span><textarea name="gc[message]" id="gc-message">{$POST.message}</textarea></span></div></div>
   <div><input type="submit" class="button" name="Submit" value="{$LANG.catalogue.add_to_basket}"{if !$ctrl_allow_purchase} disabled="disabled"{/if}></div>
 </form>
 <div class="hide" id="validate_email">{$LANG.common.error_email_invalid}</div>
