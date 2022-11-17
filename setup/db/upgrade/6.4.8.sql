e335f245f12b2c766a442a5235b9b3a840ec4f14
Al Brookbanks <al@brooky.com>
2022-11-17 07:34:44 +0000

    #3078 - Update email content condition

--- setup/db/upgrade/6.4.8.sql
+++ setup/db/upgrade/6.4.8.sql
@@ -2,5 +2,5 @@ ALTER TABLE `CubeCart_extension_info` ADD INDEX(`seller_id`); #EOQ
 ALTER TABLE `CubeCart_extension_info` ADD `keep_current` BOOLEAN NOT NULL DEFAULT FALSE AFTER `modified`; #EOQ
 ALTER TABLE `CubeCart_filemanager` ADD `alt` TEXT NOT NULL; #EOQ
 ALTER TABLE `CubeCart_order_summary` CHANGE `ship_tracking` `ship_tracking` TEXT NULL DEFAULT NULL; #EOQ
-UPDATE `CubeCart_email_content` SET `content_html` = CONCAT(`content_html`,'{if !empty($DATA.ship_tracking)}<p>Track your order:<br>{$DATA.ship_tracking}</p>{/if}') WHERE `content_type` = 'cart.order_complete'; #EOQ
-UPDATE `CubeCart_email_content` SET `content_text` = CONCAT(`content_text`,'{if !empty($DATA.ship_tracking)}\r\n\r\nTrack your order:\r\n{$DATA.ship_tracking}{/if}') WHERE `content_type` = 'cart.order_complete'; #EOQ
+UPDATE `CubeCart_email_content` SET `content_html` = CONCAT(`content_html`,'{if !empty($DATA.ship_tracking)}<p>Track your order:<br>{$DATA.ship_tracking}</p>{/if}') WHERE `content_type` = 'cart.order_complete' AND `content_html` NOT LIKE '%{$DATA.ship_tracking}%'; #EOQ
+UPDATE `CubeCart_email_content` SET `content_text` = CONCAT(`content_text`,'{if !empty($DATA.ship_tracking)}\r\n\r\nTrack your order:\r\n{$DATA.ship_tracking}{/if}') WHERE `content_type` = 'cart.order_complete' AND `content_text` NOT LIKE '%{$DATA.ship_tracking}%'; #EOQ
