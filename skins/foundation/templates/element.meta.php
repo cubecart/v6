4f0af5f0ec2e50dfc6e7bcf221b17b0be56ae3d5
Al Brookbanks <al@brooky.com>
2023-01-31 15:51:10 +0000

    #3153 - og:*

--- skins/foundation/templates/element.meta.php
+++ skins/foundation/templates/element.meta.php
@@ -3,8 +3,10 @@
 <meta name="description" content="{if isset($META_DESCRIPTION)}{$META_DESCRIPTION}{/if}">
 <meta name="robots" content="index, follow">
 <meta name="generator" content="cubecart">
-{if $FBOG}<meta property="og:image" content="{$PRODUCT.thumbnail}">
-<meta property="og:url" content="{$VAL_SELF}">{/if}
+<meta property="og:title" content="{$META_TITLE}"/>
+<meta property="og:description" content="{$META_DESCRIPTION}"/>{if isset({$PRODUCT.medium})}
+<meta property="og:image" content="{$PRODUCT.medium}"/>{/if}
+<meta property="og:url" content="{$VAL_SELF}"/>
 {if $LANGUAGES}
 {foreach from=$LANGUAGES item=language}
 <link rel="alternate" href="{$language.url}" hreflang="{if $language.code==$CONFIG.default_language}x-default{else}{$language.code}{/if}">
