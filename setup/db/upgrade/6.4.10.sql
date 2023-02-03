0fd19340bfa8c2fb4d4494acfa74fdb00bcd251a
Al Brookbanks <al@brooky.com>
2023-02-03 13:45:09 +0000

    #3158 - to null

--- setup/db/upgrade/6.4.10.sql
+++ setup/db/upgrade/6.4.10.sql
@@ -1,1 +1,13 @@
 ALTER TABLE `CubeCart_option_assign` ADD `image_id` INT NULL; #EOQ
+ALTER TABLE `CubeCart_category` CHANGE `seo_meta_title` `seo_meta_title` TEXT NULL; #EOQ
+ALTER TABLE `CubeCart_category` CHANGE `seo_meta_description` `seo_meta_description` TEXT NULL; #EOQ
+ALTER TABLE `CubeCart_category` CHANGE `seo_meta_keywords` `seo_meta_keywords` TEXT NULL; #EOQ
+ALTER TABLE `CubeCart_documents` CHANGE `seo_meta_title` `seo_meta_title` TEXT NULL; #EOQ
+ALTER TABLE `CubeCart_documents` CHANGE `seo_meta_description` `seo_meta_description` TEXT NULL; #EOQ
+ALTER TABLE `CubeCart_documents` CHANGE `seo_meta_keywords` `seo_meta_keywords` TEXT NULL; #EOQ
+ALTER TABLE `CubeCart_inventory` CHANGE `seo_meta_title` `seo_meta_title` TEXT NULL; #EOQ
+ALTER TABLE `CubeCart_inventory` CHANGE `seo_meta_description` `seo_meta_description` TEXT NULL; #EOQ
+ALTER TABLE `CubeCart_inventory` CHANGE `seo_meta_keywords` `seo_meta_keywords` TEXT NULL; #EOQ
+ALTER TABLE `CubeCart_inventory_language` CHANGE `seo_meta_title` `seo_meta_title` TEXT NULL; #EOQ
+ALTER TABLE `CubeCart_inventory_language` CHANGE `seo_meta_description` `seo_meta_description` TEXT NULL; #EOQ
+ALTER TABLE `CubeCart_inventory_language` CHANGE `seo_meta_keywords` `seo_meta_keywords` TEXT NULL; #EOQ
