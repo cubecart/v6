7ed16bbfc8da12d1e959495dfe4f28d823fd7b11
Al Brookbanks <al@brooky.com>
2023-04-21 14:37:33 +0100

    #3245 - Sales Stats (Summary)

--- admin/skins/default/templates/statistics.product.php
+++ admin/skins/default/templates/statistics.product.php
@@ -10,5 +10,36 @@
  * License:  GPL-3.0 https://www.gnu.org/licenses/quick-guide-gplv3.html
  *}
  <div id="general" class="tab_content">
-   <h3>123</h3>
+   {if $PRODUCT}
+   <h3>{$PRODUCT.name} ({$PRODUCT.product_code})</h3>
+   <table>
+    <tr>
+      {if !empty($PRODUCT.image)}<td rowspan="6"><img src="{$PRODUCT.image}" style="margin-right: 20px" /></td>{/if}
+      <td>Created</td>
+      <td>{$PRODUCT.date_added}</td>
+    </tr>
+    <tr>
+      <td>Updated</td>
+      <td>{$PRODUCT.updated}</td>
+    </tr>
+    <tr>
+      <td>First Sale</td>
+      <td>{$PRODUCT.first_sale}</td>
+    </tr>
+    <tr>
+      <td>Last Sale</td>
+      <td>{$PRODUCT.last_sale}</td>
+    </tr>
+    <tr>
+      <td>Total Sales</td>
+      <td>{$PRODUCT.total_sales}</td>
+    </tr>
+    <tr>
+      <td>Sale Interval</td>
+      <td>{$PRODUCT.sale_interval}</td>
+    </tr>
+   </table>
+   {else}
+   <p>Product not found.</p>
+   {/if}
 </div>
--- admin/sources/statistics.product.inc.php
+++ admin/sources/statistics.product.inc.php
@@ -16,5 +16,38 @@ if (!defined('CC_INI_SET')) {
 Admin::getInstance()->permissions('settings', CC_PERM_READ, true);
 $GLOBALS['gui']->addBreadcrumb('Product');
 $GLOBALS['main']->addTabControl('Product', 'general');
+
+$product = $GLOBALS['catalogue']->getProductData((int)$_GET['product_id']);
+if($product) {
+    $master_image = isset($_GET['product_id']) ? $GLOBALS['gui']->getProductImage((int)$_GET['product_id']) : '';
+    $product['image'] = $master_image;
+
+    $join = "`CubeCart_order_inventory` AS `I` INNER JOIN `CubeCart_order_summary` AS `S` ON `I`.`cart_order_id` = `S`.`Cart_order_id`";
+    $columns = 'order_date';
+    $where = '`I`.`product_id` = '.(string)$_GET['product_id'].' AND `S`.`status` IN(2, 3)';
+
+    $first_sale = $GLOBALS['db']->select($join, $columns, $where, '`S`.`order_date` ASC', 1);
+    $last_sale = $GLOBALS['db']->select($join, $columns, $where, '`S`.`order_date` DESC', 1);
+    $all_sales = $GLOBALS['db']->select($join, $columns, $where);
+
+    function secondsToTime($seconds) {
+        $dtF = new \DateTime('@0');
+        $dtT = new \DateTime("@$seconds");
+        return $dtF->diff($dtT)->format('%a days, %h hours, %i minutes and %s seconds');
+    }
+    $product['date_added'] = formatTime(strtotime($product['date_added']));
+    $product['updated'] = formatTime(strtotime($product['updated']));
+    $data = array(
+        'first_sale' => !$first_sale ? '-' : formatTime($first_sale[0]['order_date']),
+        'last_sale' => !$last_sale ? '-' : formatTime($last_sale[0]['order_date']),
+        'total_sales' => is_array($all_sales) ? count($all_sales) : 0,
+        'sale_interval' => secondsToTime(ceil((time() - strtotime($product['date_added'])) / count($all_sales)))
+    );
+
+    $GLOBALS['smarty']->assign('PRODUCT', array_merge($product, $data));
+} else {
+    $GLOBALS['smarty']->assign('PRODUCT', false);
+}
+
 $page_content = $GLOBALS['smarty']->fetch('templates/statistics.product.php');
 ?>
