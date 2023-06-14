cd5165a7bfa1680a3500c73258869de0b639df54
Al Brookbanks <al@brooky.com>
2023-06-14 12:02:30 +0100

    Update cubecart.class.php

--- classes/cubecart.class.php
+++ classes/cubecart.class.php
@@ -1805,7 +1805,7 @@ class Cubecart
                             'value'  => base64url_encode(json_encode($value)),
                             'display' => (isset($data['name'])) ? $GLOBALS['tax']->priceFormat($data['value'], true).$data['name'] : $data['desc']
                         );
-                        if (((bool)($this->_basket['shipping']['offset'] ?? false)) == $offset) {
+                        if ((int)$this->_basket['shipping']['offset'] == $offset) {
                             $offset_matched = true;
                             $option['selected'] = ' selected="selected"';
                             if ((string)$value['value'] !== (string)$this->_basket['shipping']['value']) {
