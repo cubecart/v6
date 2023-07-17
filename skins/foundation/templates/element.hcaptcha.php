7cfc34a66ca1df29fd7dce7c944992e991ce42e4
Al Brookbanks <al@brooky.com>
2023-07-17 15:26:44 +0100

    #3347 - hCaptcha completed

--- skins/foundation/templates/element.hcaptcha.php
+++ skins/foundation/templates/element.hcaptcha.php
@@ -1,9 +1,11 @@
-<div class="h-captcha" id="hCaptchaField{$ga_fid}" data-sitekey="{$CONFIG.recaptcha_public_key}" data-size="normal"></div>
-<script src="https://js.hcaptcha.com/1/api.js?onload=hCaptchaCallback{$ga_fid}" async defer></script>
-<script type="text/javascript">
-{literal}
-var hCaptchaCallback{/literal}{$ga_fid}{literal} = function() {
-    {/literal}{if $ga_fid}{literal}hcaptcha.render('hCaptchaField{/literal}{$ga_fid}{literal}', {'sitekey' : '{/literal}{$CONFIG.recaptcha_public_key}{literal}'});{/literal}{/if}{literal}
-};
-{/literal}
-</script>
+{if empty($CONFIG.recaptcha_public_key) || empty($CONFIG.recaptcha_secret_key)}
+<p>{$LANG.form.recaptcha_key_not_set}</p>
+{else}
+<div class="h-captcha" id="hCaptchaField{$ga_fid}" data-sitekey="{$CONFIG.recaptcha_public_key}" data-error-callback="onError"></div>
+<script src="https://js.hcaptcha.com/1/api.js?onload=hCaptchaCallback{$ga_fid}&render=explicit" async defer></script>
+<script type="text/javascript">{literal}
+var hCaptchaCallback{/literal}{$ga_fid}{literal} = function () {
+    var widgetID = hcaptcha.render('hCaptchaField{/literal}{$ga_fid}{literal}', { sitekey: '{/literal}{$CONFIG.recaptcha_public_key}{literal}' });
+  };
+{/literal}</script>
+{/if}
