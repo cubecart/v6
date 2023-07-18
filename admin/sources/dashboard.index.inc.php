48c698d63a305c4be9812eb3abffc808eaa2f2a4
Al Brookbanks <al@brooky.com>
2023-07-18 09:33:47 +0100

    #3358 - Query Assignment

--- admin/sources/dashboard.index.inc.php
+++ admin/sources/dashboard.index.inc.php
@@ -133,7 +133,7 @@ if (!$GLOBALS['session']->has('version_check') && $request = new Request('www.cu
         $extension_check = array();
         foreach ($extension_versions as $v) {
             $parts = explode('/',$v['dir']);
-            if($status = $GLOBALS['db']->select('CubeCart_modules', array('status'), array('module' => $parts[2], 'folder' => $parts[3]))!==false){
+            if(($status = $GLOBALS['db']->select('CubeCart_modules', array('status'), array('module' => $parts[2], 'folder' => $parts[3]))) !== false){
                 if (file_exists(CC_ROOT_DIR.$v['dir'])) {
                     if($status[0]['status']=='1') {
                         $extension_check[$v['file_id']] = $v['modified'];
