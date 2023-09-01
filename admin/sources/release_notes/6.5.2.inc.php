a0c9794efd344bcf537a5371551f867bc18f814a
Al Brookbanks <al@brooky.com>
2023-09-01 15:51:35 +0100

    Release notes tweaks

--- admin/sources/release_notes/6.5.2.inc.php
+++ admin/sources/release_notes/6.5.2.inc.php
@@ -14,12 +14,12 @@ $GLOBALS['main']->addTabControl($lang['settings']['release_notes'], 'general');
 $GLOBALS['gui']->addBreadcrumb($lang['settings']['release_notes'], currentPage(array('node')), true);
 
 $features = array(
-    '3304' => 'Back-office 404 log. Discover pages that are not found and then use the existing redirect tool to fix them.',
+    '3304' => 'Back-office 404 log. Discover external URL\'s that have no destination and use the existing redirect tool to fix them.',
 	'3131' => 'Back-office category list now shows product count.',
 	'3229' => 'Escape key now closes back-office search pull out.',
 	'3243' => 'Memory added to back-office list size (Products, Orders, Customers).',
 	'3275' => 'Administrator log to show more detailed info. e.g. The item that was edited.',
-	'3299' => 'Improved back-office request log including headers.',
+	'3299' => 'Improved back-office request log layout with header logging.',
     '3331' => '&quot;Save &amp; Reload&quot; button added to category edit add/page.',
     '3332' => 'Google Universal Analytics removed in favour of new <a href="https://www.cubecart.com/extensions/plugins/google-analytics-for-ecommerce" target="_blank">extension</a>.',
     '3346' => 'Back-office customer list to show their chosen language.',
