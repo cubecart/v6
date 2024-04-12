53d004407d091a9421ce4d7728a44ceb6a0b32ef
Al Brookbanks <al@brooky.com>
2024-04-12 10:52:51 +0100

    Powered by layout

--- admin/skins/default/templates/ccpower.php
+++ admin/skins/default/templates/ccpower.php
@@ -7,7 +7,7 @@
 <div class="powered">
 	Open Source eCommerce<br>powered by <a href="http://www.cubecart.com">CubeCart</a>
 	<ul class="logos">
-		<li><a href="https://www.gnu.org/licenses/quick-guide-gplv3.html" target="_blank"><img src="{$SKIN_VARS.admin_folder}/skins/{$SKIN_VARS.skin_folder}/images/gpl-v3-logo.png"></a></li>
+		<li><a href="https://www.gnu.org/licenses/quick-guide-gplv3.html" target="_blank"><img src="{$SKIN_VARS.admin_folder}/skins/{$SKIN_VARS.skin_folder}/images/gpl-v3-logo.svg"></a></li>
 		<li><a href="https://www.buymeacoffee.com/cubecart" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" ></a></li>
 	</ul>
 </div>
