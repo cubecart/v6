5db086756863e54f8c37c713dca0c7a24aa3383f
Al Brookbanks <al@brooky.com>
2024-04-12 10:57:09 +0100

    Tweaks

--- admin/skins/default/templates/ccpower.php
+++ admin/skins/default/templates/ccpower.php
@@ -7,7 +7,8 @@
 <div class="powered">
 	Open Source eCommerce<br>powered by <a href="http://www.cubecart.com">CubeCart</a>
 	<ul class="logos">
-		<li><a href="https://www.gnu.org/licenses/quick-guide-gplv3.html" target="_blank"><img src="{$SKIN_VARS.admin_folder}/skins/{$SKIN_VARS.skin_folder}/images/gpl-v3-logo.svg"></a></li>
+		<li><a href="https://www.gnu.org/licenses/quick-guide-gplv3.html" target="_blank"><img src="{$SKIN_VARS.admin_folder}/skins/{$SKIN_VARS.skin_folder}/images/gpl-v3-logo.svg" alt="GPLv3 logo"></a></li>
 		<li><a href="https://www.buymeacoffee.com/cubecart" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" ></a></li>
 	</ul>
+</div>
 </div>
