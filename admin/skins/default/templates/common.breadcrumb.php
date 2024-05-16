30420ad2aba754c3603fadd350bf9d4ed9324058
Al Brookbanks <al@brooky.com>
2024-05-16 10:06:13 +0100

    Promo

--- admin/skins/default/templates/common.breadcrumb.php
+++ admin/skins/default/templates/common.breadcrumb.php
@@ -2,7 +2,7 @@
    <div class="inner">
       <ul class="quick_links">
       {if !isset($CONFIG.cid)}
-      <li><i class="fa fa-server" aria-hidden="true"></i> <a href="https://hosted.cubecart.com/offer" target="_blank">Official CubeCart Hosting Offer</a></li>
+      <li><a href="https://hosted.cubecart.com/offer" target="_blank">Boost Speed &#128640;</a></li>
       {/if}
       {if $QUICK_TOUR}
       <li><a href="#" id="quickTour">{$LANG.dashboard.quick_tour}</a></li>
