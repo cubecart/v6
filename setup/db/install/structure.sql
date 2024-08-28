94a108d7bca67fb5ad475cf30ab83764d80d688e
Al Brookbanks <al@brooky.com>
2024-08-28 10:53:33 +0100

    #3633 - CubeCart_category_discount table

--- setup/db/install/structure.sql
+++ setup/db/install/structure.sql
@@ -160,6 +160,15 @@ CREATE TABLE IF NOT EXISTS `CubeCart_category` (
 	KEY `cat_parent_id` (`cat_parent_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci; #EOQ
 
+CREATE TABLE IF NOT EXISTS `CubeCart_category_discount` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `cat_id` int NOT NULL,
+  `group_id` int NOT NULL,
+  `percent` decimal(5,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cat_id` (`cat_id`,`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci; #EOQ
+
 CREATE TABLE IF NOT EXISTS `CubeCart_category_index` (
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	`cat_id` INT UNSIGNED NOT NULL DEFAULT '0',
