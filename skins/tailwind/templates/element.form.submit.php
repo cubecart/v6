7d54e7fd5c7208288a8ed522323f1596d99f6217
Al Brookbanks <al@brooky.com>
2025-01-30 11:58:52 +0000

    Removed experimental Tailwind skin

--- skins/tailwind/templates/element.form.submit.php
+++
@@ -1,5 +0,0 @@
-<div class="mt-6 flex items-center gap-x-6">
-    <button type="{if isset($type) && !empty($type)}{$type}{else}submit{/if}"{if isset($name) && !empty($name)} name="{$name}"{/if} class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{$value}</button>
-{if isset($reset) && $reset}
-    <button type="reset" class="text-sm font-semibold leading-6 text-gray-900">{$LANG.common.reset}</button>{/if}
-</div>
--- skins/tailwind/templates/element.form.textbox.php
+++
@@ -1,6 +0,0 @@
-<div class="sm:col-span-3 mt-4">
-  <label for="{$name}" class="block text-sm font-medium leading-6 text-gray-900">{$label}</label>
-  <div class="mt-2">
-    <input type="{if isset($type) && !empty($type)}{$type}{else}text{/if}" name="{$name}" id="{$name}" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6{if isset($transform) && !empty($transform)} {$transform}{/if}" value="{$value}" placeholder='{$placeholder}{if isset($required) && $required}{$LANG.common.required}{/if}'{if isset($maxlength) && !empty($maxlength)} maxlength="{$maxlength}"{/if}{if isset($autocomplete) && !empty($autocomplete)} autocomplete="{$autocomplete}"{/if}>
-  </div>
-</div>
