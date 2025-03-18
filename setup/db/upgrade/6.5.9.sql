c2c7cbab17b4ac80c24ae625118c1d9eaeab4697
Al Brookbanks <al@brooky.com>
2025-03-18 11:35:14 +0000

    #3733 - universal script

--- setup/db/upgrade/6.5.9.sql
+++ setup/db/upgrade/6.5.9.sql
@@ -1,6 +1,1 @@
 ALTER TABLE `CubeCart_customer` ADD `currency` VARCHAR(3) NULL AFTER `language`; #EOQ
-ALTER TABLE `CubeCart_customer` ENGINE=InnoDB; #EOQ
-ALTER TABLE `CubeCart_inventory` ENGINE=InnoDB; #EOQ
-ALTER TABLE `CubeCart_inventory_language` ENGINE=InnoDB; #EOQ
-ALTER TABLE `CubeCart_order_notes` ENGINE=InnoDB; #EOQ
-ALTER TABLE `CubeCart_reviews` ENGINE=InnoDB; #EOQ
--- setup/scripts/upgrade/6.5.9.php
+++ setup/scripts/upgrade/6.5.9.php
@@ -11,3 +11,9 @@ if(is_array($main)) {
         }
     }
 }
+$sql_statements = $db->misc("SELECT CONCAT('ALTER TABLE `', table_name, '` ENGINE=InnoDB;') AS sql_statement FROM information_schema.tables AS tb WHERE table_schema = '".$glob['dbdatabase']."' AND `ENGINE` = 'MyISAM' AND `TABLE_TYPE` = 'BASE TABLE' ORDER BY table_name DESC;");
+if(is_array($sql_statements) && !empty($sql_statements)) {
+    foreach($sql_statements as $s) {
+        $GLOBALS['db']->misc($s['sql_statement']);
+    }
+}
