e57fda7644efa4fade62f2b036fa22d3244814d6
Al Brookbanks <al@brooky.com>
2025-08-26 18:27:15 +0100

    #3763 - prevent running if already done

--- setup/db/upgrade/6.5.10.sql
+++ setup/db/upgrade/6.5.10.sql
@@ -1,10 +1,3 @@
 ALTER TABLE `CubeCart_filemanager` CHANGE `md5hash` `md5hash` VARCHAR(32) default NULL; #EOQ
 ALTER TABLE `CubeCart_email_log` ADD COLUMN `attachment` text NULL; #EOQ
-ALTER TABLE `CubeCart_sessions` DROP PRIMARY KEY; #EOQ
-ALTER TABLE `CubeCart_sessions` ADD KEY (`session_id`); #EOQ
-ALTER TABLE `CubeCart_sessions` ADD `id` INT UNSIGNED NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`id`); #EOQ
-ALTER TABLE `CubeCart_seo_urls` DROP PRIMARY KEY; #EOQ
-ALTER TABLE `CubeCart_seo_urls` DROP INDEX `id`;
-ALTER TABLE `CubeCart_seo_urls` ADD PRIMARY KEY(`id`); #EOQ
-ALTER TABLE `CubeCart_seo_urls`  ADD UNIQUE (`path`); #EOQ
 ALTER TABLE `CubeCart_order_history` ADD KEY (`status`); #EOQ
--- setup/scripts/upgrade/6.5.10.php
+++ setup/scripts/upgrade/6.5.10.php
@@ -3,3 +3,15 @@ $cs = $db->select('CubeCart_config', array('array'), array('name' => 'config'));
 $c = json_decode(base64_decode($cs[0]['array']), true);
 $c['standard_url']  = preg_replace('#^http://#', 'https://', $c['standard_url']);
 $db->update('CubeCart_config', array('array' => base64_encode(json_encode($c))), array('name' => 'config'));
+// We are using PHP instead of the SQL file for the following changes to avoid errors if they have already been applied
+if(!$db->misc("SHOW COLUMNS FROM `".$glob['dbprefix']."CubeCart_sessions` LIKE 'id'")) {
+    $db->misc("ALTER TABLE `".$glob['dbprefix']."CubeCart_sessions` DROP PRIMARY KEY");
+    $db->misc("ALTER TABLE `".$glob['dbprefix']."CubeCart_sessions` ADD KEY (`session_id`)");
+    $db->misc("ALTER TABLE `".$glob['dbprefix']."CubeCart_sessions` ADD `id` INT UNSIGNED NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`id`)");
+}
+if(!$db->misc("SHOW COLUMNS FROM `".$glob['dbprefix']."CubeCart_seo_urls` LIKE 'id'")) {
+    $db->misc("ALTER TABLE `".$glob['dbprefix']."CubeCart_seo_urls` DROP PRIMARY KEY");
+    $db->misc("ALTER TABLE `".$glob['dbprefix']."CubeCart_seo_urls` DROP INDEX `id`");
+    $db->misc("ALTER TABLE `".$glob['dbprefix']."CubeCart_seo_urls` ADD PRIMARY KEY(`id`)");
+    $db->misc("ALTER TABLE `".$glob['dbprefix']."CubeCart_seo_urls` ADD UNIQUE (`path`)");
+}
