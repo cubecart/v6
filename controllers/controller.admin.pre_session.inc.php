393b369d66164d21c6b8c028f971b52852597ff5
Al Brookbanks <al@brooky.com>
2026-02-25 19:18:25 +0000

    #3897 - "html_entity_decode" as a modifier

--- controllers/controller.admin.pre_session.inc.php
+++ controllers/controller.admin.pre_session.inc.php
@@ -35,6 +35,7 @@ Sanitize::checkToken();
 // Initialize Smarty
 $GLOBALS['smarty'] = new Smarty();
 $GLOBALS['smarty']->muteUndefinedOrNullWarnings();
+$GLOBALS['smarty']->registerPlugin('modifier', 'html_entity_decode', 'html_entity_decode');
 $GLOBALS['smarty']->error_reporting = E_ALL & ~E_NOTICE & ~E_WARNING;
 $GLOBALS['smarty']->compile_dir  = CC_SKIN_CACHE_DIR;
 $GLOBALS['smarty']->config_dir  = CC_SKIN_CACHE_DIR;
