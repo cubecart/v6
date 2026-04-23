78f967d8fa408bf69f2610a2eb20d6af5c380855
Al Brookbanks <al@brooky.com>
2026-04-23 11:21:23 +0100

    #4030 - Clear Cache

--- admin/skins/default/templates/common.breadcrumb.php
+++ admin/skins/default/templates/common.breadcrumb.php
@@ -5,7 +5,6 @@
       {if $QUICK_TOUR}
       <li><a href="#" id="quickTour">{$LANG.dashboard.quick_tour}</a></li>
       {/if}
-      {if $CONFIG.cache}<li id="clear_cache_master"{if $CLEAR_CACHE} class="clear"{/if}><a href="{$SKIN_VARS.clear_cache_link}">{$LANG.maintain.cache_clear}</a></li>{/if}
       <li id="help_menu"><i class="fa fa-life-ring" aria-hidden="true"></i> <a href="#">{$LANG.common.help}</a>
          <ul>
             {if $HELP_URL}<li><i class="fa fa-book" aria-hidden="true"></i> <a href="{$HELP_URL}" id="wikihelp" class="help-panel-trigger">{$LANG.common.this_page}</a></li>{/if}
