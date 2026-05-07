7575c971bcf61fa238c19d606f59e2e90b03692f
Al Brookbanks <al@brooky.com>
2026-05-07 07:59:30 +0100

    Added images/source

---
+++ images/source/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * CubeCart v6
+ * ========================================
+ * CubeCart is a registered trade mark of CubeCart Limited
+ * Copyright CubeCart Limited 2026. All rights reserved.
+ * UK Private Limited Company No. 5323904
+ * ========================================
+ * Web:   https://www.cubecart.com
+ * Email:  hello@cubecart.com
+ * License:  GPL-3.0 https://www.gnu.org/licenses/quick-guide-gplv3.html
+ */
+die('Access denied');
