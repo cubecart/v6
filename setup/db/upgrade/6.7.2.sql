0115c5507cef89399f7501f1324e5afce63b4034
Al Brookbanks <al@brooky.com>
2026-05-08 11:26:39 +0100

    #4113 - title cols

---
+++ setup/db/upgrade/6.7.2.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `CubeCart_addressbook`    ADD COLUMN `title`   VARCHAR(8) DEFAULT NULL; #EOQ
+ALTER TABLE `CubeCart_customer`       ADD COLUMN `title`   VARCHAR(8) DEFAULT NULL; #EOQ
+ALTER TABLE `CubeCart_order_summary`  ADD COLUMN `title`   VARCHAR(8) DEFAULT NULL; #EOQ
+ALTER TABLE `CubeCart_order_summary`  ADD COLUMN `title_d` VARCHAR(8) DEFAULT NULL; #EOQ
